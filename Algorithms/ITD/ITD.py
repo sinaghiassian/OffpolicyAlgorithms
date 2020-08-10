ef4b4dd647c355f8801cde4196faa5466dcdf98f
sinaghiassian <ghiassia@ualberta.ca>
2020-08-10 09:09:14 -0600

    Deleted nonsense test algorithm

--- Algorithms/ITD/ITD.py
+++
@@ -1,9 +0,0 @@
-from Algorithms.TD.TD import TD
-import numpy as np
-
-
-class ITD(TD):
-    def compute_step_size(self, transition):
-        step_size = self.alpha / (1 + self.alpha * np.dot(
-            (transition.x - self.gamma * transition.xp), transition.x) * transition.x)
-        return step_size
