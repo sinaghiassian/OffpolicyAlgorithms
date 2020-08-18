b9ab31584ecf16d3ebc2f49ea6e11700c9e68e5b
sinaghiassian <ghiassia@ualberta.ca>
2020-08-18 11:36:12 -0600

    Added get_feat_rep

--- Learning.py
+++ Learning.py
@@ -28,4 +28,4 @@ if __name__ == '__main__':
 
     alg_dict = {'TD': TD}
     agent = alg_dict[args.algorithm]()
-
+    
--- Problems/EightStateOffPolicyRandomFeat.py
+++ Problems/EightStateOffPolicyRandomFeat.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-class EightStateOffPolicyRandomFeat():
+class EightStateOffPolicyRandomFeat:
     def __init__(self, n=8):
         self.N = n
         self.num_features = 6
@@ -10,6 +10,14 @@ class EightStateOffPolicyRandomFeat():
         self.GAMMA = 0.9
         self.behavior_dist = np.zeros(self.N + 1)
         self.state_values = np.zeros(self.N + 1)
+
+    def create_feature_rep(self):
+        num_ones = 3
+        num_zeros = self.num_features - num_ones
+        for i in range(self.N):
+            random_arr = (np.array([0] * num_zeros + [1] * num_ones))
+            np.random.shuffle(random_arr)
+            self.feature_rep[i, :] = random_arr
 
     @property
     def get_num_steps(self):
@@ -28,3 +36,10 @@ class EightStateOffPolicyRandomFeat():
     def get_state_value(self):  # TODO: This is just a placeholder for now.
         self.state_values = np.array([0, 0, 0])
         return self.behavior_dist
+
+    @property
+    def get_feat_rep(self, run):
+        return np.load('feature_rep.npy')[:, :, run]  # Todo: Clean this up.
+        # self.create_feature_rep()
+        # return self.feature_rep
+
