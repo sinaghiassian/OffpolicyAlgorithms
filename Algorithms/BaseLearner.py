15a8fbc56557efb58f2ffcfe273433c0f3e9da64
sinaghiassian <ghiassia@ualberta.ca>
2020-08-19 12:45:02 -0600

    Moved Select Action functions to BaseLearner.py

--- Algorithms/BaseLearner.py
+++ Algorithms/BaseLearner.py
@@ -1,21 +1,26 @@
 import numpy as np
 
+from Problems.BaseProblem import BaseProblem
+
 
 class BaseLearner:
-    def __init__(self, **kwargs):
+    def __init__(self, problem: BaseProblem, **kwargs):
+        self.problem = problem
         self.w = np.zeros(kwargs['num_features'])
         self.gamma = kwargs['gamma']
         self.alpha = kwargs['alpha']
         self.lmbda = kwargs['lmbda']
+        self.feature_rep = np.load('feature_rep.npy')[:, :, kwargs['run']]
+        self.state = -1
 
     def compute_step_size(self, transition):
         return self.alpha
 
-    def learn(self, *args):
-        raise NotImplementedError
-
     def choose_behavior_action(self):
-        raise NotImplementedError
+        return self.problem.select_behavior_action(self.feature_rep[self.state, :])
 
     def choose_target_action(self):
+        return self.problem.select_target_action(self.feature_rep[self.state, :])
+
+    def learn(self, *args):
         raise NotImplementedError
