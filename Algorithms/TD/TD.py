0441581bf73742fd36765a6f83ee4ba636d58fca
sinaghiassian <ghiassia@ualberta.ca>
2020-08-23 18:53:02 -0600

    Moved compute_rmsve function to BaseAgent.py instead of Learning.py.

--- Algorithms/TD/TD.py
+++ Algorithms/TD/TD.py
@@ -3,6 +3,8 @@ import numpy as np
 
 
 class TD(BaseAgent):
-    def learn(self, transition):
-        delta = transition.r + self.gamma * np.dot(self.w, transition.xp) - np.dot(self.w, transition.x)
-        self.w += self.compute_step_size(transition) * delta * transition.x
+    def learn(self, s, s_p, r):
+        x_p = self.problem.get_state_feature_rep(s_p)
+        x = self.problem.get_state_feature_rep(s)
+        delta = r + self.gamma * np.dot(self.w, x_p) - np.dot(self.w, x)
+        self.w += self.compute_step_size() * delta * x
--- Problems/EightStateOffPolicyRandomFeat.py
+++ Problems/EightStateOffPolicyRandomFeat.py
@@ -1,13 +1,14 @@
 import numpy as np
 
+from Environments.Chain import Chain
 from Problems.BaseProblem import BaseProblem
 
 
-class EightStateOffPolicyRandomFeat(BaseProblem):
+class EightStateOffPolicyRandomFeat(BaseProblem, Chain):
     def __init__(self, n=8):
+        super(BaseProblem).__init__()
         self.N = n
         self.num_features = 6
-        self.feature_rep = np.zeros((self.N + 1, self.num_features))
         self.num_steps = 5000
         self.GAMMA = 0.9
         self.behavior_dist = np.zeros(self.N + 1)
@@ -21,6 +22,9 @@ class EightStateOffPolicyRandomFeat(BaseProblem):
             np.random.shuffle(random_arr)
             self.feature_rep[i, :] = random_arr
 
+    def get_state_feature_rep(self, state):
+        return self.feature_rep[state, :, self.run_number]
+
     @property
     def get_behavior_dist(self):
         self.behavior_dist = np.load('Resource/d_mu.npy')
@@ -30,3 +34,12 @@ class EightStateOffPolicyRandomFeat(BaseProblem):
     def get_state_value(self):
         self.state_values = np.load('Resource/state_values.npy')
         return self.state_values
+
+    def select_behavior_action(self, s):
+        if s < self.N / 2:
+            return self.RIGHT_ACTION
+        else:
+            return np.random.choice([self.RIGHT_ACTION, self.RETREAT_ACTION])
+
+    def select_target_action(self, s):
+        return self.RIGHT_ACTION
