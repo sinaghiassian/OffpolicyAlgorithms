225ed7666fa4ac4a840afe2a059cada9a5214060
ali <akyegane@gmail.com>
2020-09-01 15:14:10 +0430

    remove redundant codes from environment

--- Environments/FourRoomGridWorld.py
+++ Environments/FourRoomGridWorld.py
@@ -96,34 +96,3 @@ class FourRoomGridWorld(gym.Env):
         }
         move_func = switcher.get(action)
         return move_func(x, y)
-
-
-if __name__ == "__main__":
-    actions = {
-        0: 'up',
-        1: 'down',
-        2: 'right',
-        3: 'left',
-    }
-    env = FourRoomGridWorld()
-    env.reset()
-    env.render()
-    is_terminal = False
-    state = env.reset()
-    for step in range(40):
-        a = np.random.randint(0, 3)
-        next_state, r, is_terminal, info = env.step(a)
-        x, y, is_rand, selected_action = info.values()
-        print(
-            f'sept:{step}, '
-            f'state:({state[0]},{state[1]}), '
-            f'action: {actions[a]}, '
-            f'environment_action: {actions[selected_action]}, '
-            f'next_state:({next_state[0]},{next_state[1]}), '
-            f'stochasticity:{is_rand}, '
-            f'terminal:{is_terminal}'
-        )
-        state = next_state
-        env.render()
-        if is_terminal:
-            break
