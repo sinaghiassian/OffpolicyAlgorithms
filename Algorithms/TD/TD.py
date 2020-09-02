2bd7517c40f9a94d4f926b5c22abb87cbf064b06
sinaghiassian <ghiassia@ualberta.ca>
2020-09-02 15:04:40 -0600

    Completed the first implementation of the TD agent for learning multiple policies at the same time.

--- Algorithms/TD/TD.py
+++ Algorithms/TD/TD.py
@@ -16,13 +16,15 @@ class TD(BaseAgent):
 
 class TDMultiplePolicy(BaseAgentLearnMultiplePolicies):
     def learn(self, s, s_p, r):
+        active_policies_vec = self.problem.get_active_policies(s_p)
+        alpha_vec = active_policies_vec * self.compute_step_size()
         x = self.problem.get_state_feature_rep(s)
         x_p = self.problem.get_state_feature_rep(s_p)
         stacked_x = self.problem.stacked_feature_rep[s]
-        stacked_x_p = self.problem.stacked_feature_rep[s_p]
         pi = self.problem.get_pi(s, self.action)
         mu = self.problem.get_mu(s, self.action)
         rho = pi / mu
-        delta = r + rho * np.dot(self.w, x_p) - np.dot(self.w, x)
-        self.z = rho[:, None] * (self.lmbda * self.z * self.gamma_vec)
-        active_policies = self.problem.get_active_policies(s) * self.gamma
+        delta = r + np.dot(self.w, x_p) - np.dot(self.w, x)
+        gamma_vec = self.problem.get_active_policies(s) * self.gamma
+        self.z = rho[:, None] * (self.lmbda * self.z * gamma_vec + stacked_x)
+        self.w = self.w + (alpha_vec * delta)[:, None] * self.z
