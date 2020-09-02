32493d3fb90e22f2acbb74cf40376c7a1bd3b2a7
sinaghiassian <ghiassia@ualberta.ca>
2020-09-01 19:20:17 -0600

    Added stacked feature rep to base problem. Also added functions for getting active policies and stacking the feautre representation to learn multiple policies at the same time.

--- Problems/BaseProblem.py
+++ Problems/BaseProblem.py
@@ -1,4 +1,5 @@
 from abc import abstractmethod
+import numpy as np
 
 
 class BaseProblem:
@@ -6,11 +7,23 @@ class BaseProblem:
         self.run_number = run_number
         self.num_steps = 5000
         self.feature_rep = None
+        self.stacked_feature_rep = None  # If learning more than one target policy at the same time
         self.num_features = None
         self.GAMMA = None
         self.behavior_dist = None
         self.state_values = None
         self.num_policies = None
+
+    @abstractmethod
+    def get_active_policies(self, s):
+        raise NotImplementedError
+
+    def stack_feature_rep(self):
+        stacked_feature_rep = np.zeros((self.num_policies, *self.feature_rep.shape))
+        for i in range(self.feature_rep.shape[0]):
+            stacked_x = np.tile(self.feature_rep[i, :], [self.num_policies, 1])
+            stacked_feature_rep[:, :, i] = stacked_x
+        return stacked_feature_rep
 
     @abstractmethod
     def load_feature_rep(self):
