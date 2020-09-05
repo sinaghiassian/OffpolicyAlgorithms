cedec4e66624f5d7af792ba3c42929e5ded522c8
sinaghiassian <ghiassia@ualberta.ca>
2020-09-05 11:35:08 -0600

    Debugged TD.py.

--- Algorithms/TD/TD.py
+++ Algorithms/TD/TD.py
@@ -16,7 +16,11 @@ class TD(BaseAgent):
 
 class TDMultiplePolicy(BaseAgentLearnMultiplePolicies):
     def learn(self, s, s_p, r):
-        active_policies_vec = self.problem.get_active_policies(s_p)
+        active_policies_vec = self.problem.get_active_policies(s)
+        self.r_vec = np.zeros(self.problem.num_policies)
+        if r > 0:
+            terminal_policies_vec = self.problem.get_terminal_policies(s_p)
+            self.r_vec = r * terminal_policies_vec
         alpha_vec = active_policies_vec * self.compute_step_size()
         x = self.problem.get_state_feature_rep(s)
         x_p = self.problem.get_state_feature_rep(s_p)
@@ -24,7 +28,7 @@ class TDMultiplePolicy(BaseAgentLearnMultiplePolicies):
         pi = self.problem.get_pi(s, self.action)
         mu = self.problem.get_mu(s, self.action)
         rho = pi / mu
-        delta = r + np.dot(self.w, x_p) - np.dot(self.w, x)
-        gamma_vec = self.problem.get_active_policies(s) * self.gamma
+        delta = self.r_vec + np.dot(self.w, x_p) - np.dot(self.w, x)
+        gamma_vec = self.problem.get_active_policies(s_p) * self.gamma
         self.z = rho[:, None] * (self.lmbda * self.z * gamma_vec[:, None] + stacked_x)
-        self.w = self.w + (alpha_vec * delta)[:, None] * self.z
+        self.w += (alpha_vec * delta)[:, None] * self.z
