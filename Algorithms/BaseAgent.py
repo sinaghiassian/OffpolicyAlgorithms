83949687d8032539caa5e52f067f7117fe614915
sinaghiassian <ghiassia@ualberta.ca>
2020-09-07 11:17:24 -0600

    Changed RMSVE to make it compatible with learning multiple and single policy. Also removed BaseAgentMultiplePolicies. Now we only have one BaseAgent class.

--- Algorithms/BaseAgent.py
+++ Algorithms/BaseAgent.py
@@ -7,39 +7,6 @@ class BaseAgent:
     def __init__(self, problem: BaseProblem, **kwargs):
         self.problem = problem
         self.w = np.zeros(self.problem.num_features)
-        self.gamma = kwargs['GAMMA']
-        self.alpha = kwargs['alpha']
-        self.lmbda = kwargs['lmbda']
-        self.state_values = self.problem.load_state_values()
-        self.d_mu = self.problem.load_behavior_dist()
-        self.state, self.next_state, self.action = None, None, None
-
-    def compute_rmsve(self):
-        est_value = np.dot(self.problem.feature_rep[:-1, :], self.w)
-        error = (est_value - self.state_values[:-1])
-        error_squared = error * error
-        return np.sqrt(np.sum(self.d_mu[:-1] * error_squared))
-
-    def compute_step_size(self):
-        return self.alpha
-
-    def choose_behavior_action(self):
-        return self.problem.select_behavior_action(self.state)
-
-    def choose_target_action(self):
-        return self.problem.select_target_action(self.state)
-
-    def learn(self, *args):
-        raise NotImplementedError
-
-    def __str__(self):
-        return f'agent:{__name__}'
-
-
-class BaseAgentLearnMultiplePolicies:
-    def __init__(self, problem: BaseProblem, **kwargs):
-        self.problem = problem
-        self.w = np.zeros(self.problem.num_features)
         self.z = np.zeros(self.problem.num_features)
         if self.problem.num_policies > 1:
             self.w = np.zeros((self.problem.num_policies, self.problem.num_features))
@@ -58,7 +25,13 @@ class BaseAgentLearnMultiplePolicies:
         est_value = np.dot(self.w, self.problem.feature_rep.T)
         error = est_value - self.state_values
         error_squared = error * error
-        return np.sqrt(np.sum(self.d_mu * error_squared, 1) / np.sum(self.d_mu, 1))
+        return np.sqrt(np.sum(self.d_mu * error_squared.T, 0) / np.sum(self.d_mu, 0))
+
+    def compute_rmsve_old(self):
+        est_value = np.dot(self.problem.feature_rep[:-1, :], self.w)
+        error = (est_value - self.state_values[:-1])
+        error_squared = error * error
+        return np.sqrt(np.sum(self.d_mu[:-1] * error_squared))
 
     def compute_step_size(self):
         return self.alpha
@@ -69,7 +42,16 @@ class BaseAgentLearnMultiplePolicies:
     def choose_target_action(self):
         return self.problem.select_target_action(self.state)
 
-    def learn(self, *args):
+    def learn(self, s, s_p, r, is_terminal):
+        if self.problem.num_policies == 1:
+            self.learn_single_policy(s, s_p, r, is_terminal)
+        else:
+            self.learn_multiple_policies(s, s_p, r, is_terminal)
+
+    def learn_single_policy(self, s, s_p, r, is_terminal):
+        raise NotImplementedError
+
+    def learn_multiple_policies(self, s, s_p, r, is_terminal):
         raise NotImplementedError
 
     def __str__(self):
