38df5a6957cb18c068e55f983956d3dd4bb1a8cc
ali <akyegane@gmail.com>
2020-09-07 12:05:24 +0430

    string conditions in policies is replaced by lambda in order to remove time consuming eval built in function

--- Problems/LearnEightPoliciesTileCodingFeat.py
+++ Problems/LearnEightPoliciesTileCodingFeat.py
@@ -12,7 +12,7 @@ class LearnEightPoliciesTileCodingFeat(BaseProblem, FourRoomGridWorld):
         FourRoomGridWorld.__init__(self)
         self.feature_rep = self.load_feature_rep()
         self.num_features = self.feature_rep.shape[1]
-        self.num_steps = 1000
+        self.num_steps = 10000
         self.GAMMA = 0.9
         self.behavior_dist = self.load_behavior_dist()
         self.state_values = self.load_state_values()
@@ -20,68 +20,68 @@ class LearnEightPoliciesTileCodingFeat(BaseProblem, FourRoomGridWorld):
         self.optimal_policies = ImmutableDict(
             {
                 0: [
-                    ['0 <= x <= 3 and 2 <= y <= 4', [self.ACTION_DOWN, self.ACTION_RIGHT]],
-                    ['3 >= x >= 0 == y', [self.ACTION_UP, self.ACTION_RIGHT]],
-                    ['0 <= x <= 4 and y == 1', [self.ACTION_RIGHT]],
-                    ['x == hall[1][0] and y == hall[1][1]', [self.ACTION_DOWN]],
-                    ['4 == x and 2 <= y <= 4', [self.ACTION_DOWN]],
-                    ['4 == x and y == 0', [self.ACTION_UP]]
+                    [lambda x, y: 0 <= x <= 3 and 2 <= y <= 4, [self.ACTION_DOWN, self.ACTION_RIGHT]],
+                    [lambda x, y: 3 >= x >= 0 == y, [self.ACTION_UP, self.ACTION_RIGHT]],
+                    [lambda x, y: 0 <= x <= 4 and y == 1, [self.ACTION_RIGHT]],
+                    [lambda x, y: x == self.hallways[1][0] and y == self.hallways[1][1], [self.ACTION_DOWN]],
+                    [lambda x, y: 4 == x and 2 <= y <= 4, [self.ACTION_DOWN]],
+                    [lambda x, y: 4 == x and y == 0, [self.ACTION_UP]]
                 ],
                 1: [
-                    ['2 <= x <= 4 and 0 <= y <= 3', [self.ACTION_LEFT, self.ACTION_UP]],
-                    ['x == 0 and 0 <= y <= 3', [self.ACTION_RIGHT, self.ACTION_UP]],
-                    ['x == 1 and 0 <= y <= 4', [self.ACTION_UP]],
-                    ['x == hall[0][0] and y == hall[0][1]', [self.ACTION_LEFT]],
-                    ['2 <= x <= 4 and y == 4', [self.ACTION_LEFT]],
-                    ['x == 0 and y == 4', [self.ACTION_RIGHT]],
+                    [lambda x, y: 2 <= x <= 4 and 0 <= y <= 3, [self.ACTION_LEFT, self.ACTION_UP]],
+                    [lambda x, y: x == 0 and 0 <= y <= 3, [self.ACTION_RIGHT, self.ACTION_UP]],
+                    [lambda x, y: x == 1 and 0 <= y <= 4, [self.ACTION_UP]],
+                    [lambda x, y: x == self.hallways[0][0] and y == self.hallways[0][1], [self.ACTION_LEFT]],
+                    [lambda x, y: 2 <= x <= 4 and y == 4, [self.ACTION_LEFT]],
+                    [lambda x, y: x == 0 and y == 4, [self.ACTION_RIGHT]],
                 ],
                 2: [
-                    ['2 <= x <= 4 and 7 <= y <= 10', [self.ACTION_LEFT, self.ACTION_DOWN]],
-                    ['x == 0 and 7 <= y <= 10', [self.ACTION_RIGHT, self.ACTION_DOWN]],
-                    ['x == 1 and 6 <= y <= 10', [self.ACTION_DOWN]],
-                    ['x == hall[2][0] and y == hall[2][1]', [self.ACTION_LEFT]],
-                    ['2 <= x <= 4 and y == 6', [self.ACTION_LEFT]],
-                    ['x == 0 and y == 6', [self.ACTION_RIGHT]],
+                    [lambda x, y: 2 <= x <= 4 and 7 <= y <= 10, [self.ACTION_LEFT, self.ACTION_DOWN]],
+                    [lambda x, y: x == 0 and 7 <= y <= 10, [self.ACTION_RIGHT, self.ACTION_DOWN]],
+                    [lambda x, y: x == 1 and 6 <= y <= 10, [self.ACTION_DOWN]],
+                    [lambda x, y: x == self.hallways[2][0] and y == self.hallways[2][1], [self.ACTION_LEFT]],
+                    [lambda x, y: 2 <= x <= 4 and y == 6, [self.ACTION_LEFT]],
+                    [lambda x, y: x == 0 and y == 6, [self.ACTION_RIGHT]],
                 ],
                 3: [
-                    ['0 <= x <= 3 and 6 <= y <= 7', [self.ACTION_UP, self.ACTION_RIGHT]],
-                    ['0 <= x <= 3 and 9 <= y <= 10', [self.ACTION_DOWN, self.ACTION_RIGHT]],
-                    ['0 <= x <= 4 and y == 8', [self.ACTION_RIGHT]],
-                    ['x == hall[1][0] and y == hall[1][1]', [self.ACTION_UP]],
-                    ['x == 4 and 6 <= y <= 7', [self.ACTION_UP]],
-                    ['x == 4 and 9 <= y <= 10', [self.ACTION_DOWN]]
+                    [lambda x, y: 0 <= x <= 3 and 6 <= y <= 7, [self.ACTION_UP, self.ACTION_RIGHT]],
+                    [lambda x, y: 0 <= x <= 3 and 9 <= y <= 10, [self.ACTION_DOWN, self.ACTION_RIGHT]],
+                    [lambda x, y: 0 <= x <= 4 and y == 8, [self.ACTION_RIGHT]],
+                    [lambda x, y: x == self.hallways[1][0] and y == self.hallways[1][1], [self.ACTION_UP]],
+                    [lambda x, y: x == 4 and 6 <= y <= 7, [self.ACTION_UP]],
+                    [lambda x, y: x == 4 and 9 <= y <= 10, [self.ACTION_DOWN]]
                 ],
                 4: [
-                    ['10 >= x >= 7 >= y and y >= 5', [self.ACTION_LEFT, self.ACTION_UP]],
-                    ['7 <= x <= 10 and 9 <= y <= 10', [self.ACTION_LEFT, self.ACTION_DOWN]],
-                    ['6 <= x <= 10 and y == 8', [self.ACTION_LEFT]],
-                    ['x == hall[3][0] and y == hall[3][1]', [self.ACTION_UP]],
-                    ['x == 6 and 5 <= y <= 7', [self.ACTION_UP]],
-                    ['x == 6 and 9 <= y <= 10', [self.ACTION_DOWN]]
+                    [lambda x, y: 10 >= x >= 7 >= y >= 5, [self.ACTION_LEFT, self.ACTION_UP]],
+                    [lambda x, y: 7 <= x <= 10 and 9 <= y <= 10, [self.ACTION_LEFT, self.ACTION_DOWN]],
+                    [lambda x, y: 6 <= x <= 10 and y == 8, [self.ACTION_LEFT]],
+                    [lambda x, y: x == self.hallways[3][0] and y == self.hallways[3][1], [self.ACTION_UP]],
+                    [lambda x, y: x == 6 and 5 <= y <= 7, [self.ACTION_UP]],
+                    [lambda x, y: x == 6 and 9 <= y <= 10, [self.ACTION_DOWN]]
                 ],
                 5: [
-                    ['6 <= x <= 7 and 6 <= y <= 10', [self.ACTION_RIGHT, self.ACTION_DOWN]],
-                    ['9 <= x <= 10 and 6 <= y <= 10', [self.ACTION_DOWN, self.ACTION_LEFT]],
-                    ['x == 8 and 5 <= y <= 10', [self.ACTION_DOWN]],
-                    ['x == hall[2][0] and y == hall[2][1]', [self.ACTION_RIGHT]],
-                    ['6 <= x <= 7 and y == 5', [self.ACTION_RIGHT]],
-                    ['9 <= x <= 10 and y == 5', [self.ACTION_LEFT]]
+                    [lambda x, y: 6 <= x <= 7 and 6 <= y <= 10, [self.ACTION_RIGHT, self.ACTION_DOWN]],
+                    [lambda x, y: 9 <= x <= 10 and 6 <= y <= 10, [self.ACTION_DOWN, self.ACTION_LEFT]],
+                    [lambda x, y: x == 8 and 5 <= y <= 10, [self.ACTION_DOWN]],
+                    [lambda x, y: x == self.hallways[2][0] and y == self.hallways[2][1], [self.ACTION_RIGHT]],
+                    [lambda x, y: 6 <= x <= 7 and y == 5, [self.ACTION_RIGHT]],
+                    [lambda x, y: 9 <= x <= 10 and y == 5, [self.ACTION_LEFT]]
                 ],
                 6: [
-                    ['6 <= x <= 7 and 0 <= y <= 2', [self.ACTION_UP, self.ACTION_RIGHT]],
-                    ['9 <= x <= 10 and 0 <= y <= 2', [self.ACTION_UP, self.ACTION_LEFT]],
-                    ['x == 8 and 0 <= y <= 3', [self.ACTION_UP]],
-                    ['x == hall[0][0] and y == hall[0][1]', [self.ACTION_RIGHT]],
-                    ['6 <= x <= 7 and y == 3', [self.ACTION_RIGHT]],
-                    ['9 <= x <= 10 and y == 3', [self.ACTION_LEFT]]
+                    [lambda x, y: 6 <= x <= 7 and 0 <= y <= 2, [self.ACTION_UP, self.ACTION_RIGHT]],
+                    [lambda x, y: 9 <= x <= 10 and 0 <= y <= 2, [self.ACTION_UP, self.ACTION_LEFT]],
+                    [lambda x, y: x == 8 and 0 <= y <= 3, [self.ACTION_UP]],
+                    [lambda x, y: x == self.hallways[0][0] and y == self.hallways[0][1], [self.ACTION_RIGHT]],
+                    [lambda x, y: 6 <= x <= 7 and y == 3, [self.ACTION_RIGHT]],
+                    [lambda x, y: 9 <= x <= 10 and y == 3, [self.ACTION_LEFT]]
                 ],
                 7: [
-                    ['7 <= x <= 10 and 2 <= y <= 3', [self.ACTION_DOWN, self.ACTION_LEFT]],
-                    ['7 <= x <= 10 and y == 0', [self.ACTION_UP, self.ACTION_LEFT]],
-                    ['6 <= x <= 10 and y == 1', [self.ACTION_LEFT]],
-                    ['x == hall[3][0] and y == hall[3][1]', [self.ACTION_DOWN]],
-                    ['x == 6 and 2 <= y <= 3', [self.ACTION_DOWN]],
-                    ['x == 6 and y == 0', [self.ACTION_UP]]
+                    [lambda x, y: 7 <= x <= 10 and 2 <= y <= 3, [self.ACTION_DOWN, self.ACTION_LEFT]],
+                    [lambda x, y: 7 <= x <= 10 and y == 0, [self.ACTION_UP, self.ACTION_LEFT]],
+                    [lambda x, y: 6 <= x <= 10 and y == 1, [self.ACTION_LEFT]],
+                    [lambda x, y: x == self.hallways[3][0] and y == self.hallways[3][1], [self.ACTION_DOWN]],
+                    [lambda x, y: x == 6 and 2 <= y <= 3, [self.ACTION_DOWN]],
+                    [lambda x, y: x == 6 and y == 0, [self.ACTION_UP]]
                 ]
             }
         )
@@ -99,18 +99,19 @@ class LearnEightPoliciesTileCodingFeat(BaseProblem, FourRoomGridWorld):
         )
         self.policy_terminal_condition = ImmutableDict(
             {
-                0: 'x == hall[0][0] and y == hall[0][1]',
-                1: 'x == hall[1][0] and y == hall[1][1]',
-                2: 'x == hall[1][0] and y == hall[1][1]',
-                3: 'x == hall[2][0] and y == hall[2][1]',
-                4: 'x == hall[2][0] and y == hall[2][1]',
-                5: 'x == hall[3][0] and y == hall[3][1]',
-                6: 'x == hall[3][0] and y == hall[3][1]',
-                7: 'x == hall[0][0] and y == hall[0][1]'
+                0: lambda x, y: x == self.hallways[0][0] and y == self.hallways[0][1],
+                1: lambda x, y: x == self.hallways[1][0] and y == self.hallways[1][1],
+                2: lambda x, y: x == self.hallways[1][0] and y == self.hallways[1][1],
+                3: lambda x, y: x == self.hallways[2][0] and y == self.hallways[2][1],
+                4: lambda x, y: x == self.hallways[2][0] and y == self.hallways[2][1],
+                5: lambda x, y: x == self.hallways[3][0] and y == self.hallways[3][1],
+                6: lambda x, y: x == self.hallways[3][0] and y == self.hallways[3][1],
+                7: lambda x, y: x == self.hallways[0][0] and y == self.hallways[0][1]
             }
         )
         self.num_policies = len(self.optimal_policies)
         self.stacked_feature_rep = self.stack_feature_rep()
+        self._active_policies_cache = {}
 
     def get_terminal_policies(self, s):
         x, y = self.get_xy(s)
@@ -124,7 +125,8 @@ class LearnEightPoliciesTileCodingFeat(BaseProblem, FourRoomGridWorld):
         return int(y * np.sqrt(self.feature_rep.shape[0]) + x)
 
     def _eval(self, condition, x, y):
-        return eval(condition, {'x': x, 'y': y, 'hall': self.hallways})
+        return condition(x, y)
+        # return eval(condition, {'x': x, 'y': y, 'hall': self.hallways})
 
     def get_probability(self, policy_number, s, a):
         x, y = self.get_xy(s)
@@ -146,6 +148,8 @@ class LearnEightPoliciesTileCodingFeat(BaseProblem, FourRoomGridWorld):
         return a
 
     def get_active_policies(self, s):
+        if s in self._active_policies_cache:
+            return self._active_policies_cache[s]
         x, y = self.get_xy(s)
         active_policy_vec = np.zeros(self.num_policies)
         for policy_number, policy_values in self.optimal_policies.items():
@@ -153,6 +157,7 @@ class LearnEightPoliciesTileCodingFeat(BaseProblem, FourRoomGridWorld):
                 if self._eval(condition, x, y):
                     active_policy_vec[policy_number] = 1
                     break
+        self._active_policies_cache[s] = active_policy_vec
         return active_policy_vec
 
     def load_feature_rep(self):
