fe299ab85e7de89d160009eae7367ae157bf9277
sinaghiassian <ghiassia@ualberta.ca>
2020-09-07 11:25:33 -0600

    We now have two TD agents. One for learning multiple policies and one for learning a single policy. Both are part of a single class called TD. Inside the learn_single_policy, we now have an is_terminal variable that handles learning when a terminal state is reached. Inside the learn_multiple_policies, we now have an is_terminal variable. However, the current problem that we have is continuing and we don't have a terminal state. Instead we are controlling the learning using the GVF framework (using the discount factor gamma and in this exceptional case alpha--usually alpha should not be used for this purpose).

--- Algorithms/TD/TD.py
+++ Algorithms/TD/TD.py
@@ -1,21 +1,22 @@
-from Algorithms.BaseAgent import BaseAgent, BaseAgentLearnMultiplePolicies
+from Algorithms.BaseAgent import BaseAgent
 import numpy as np
 
 
 class TD(BaseAgent):
-    def learn(self, s, s_p, r):
+    def learn_single_policy(self, s, s_p, r, is_terminal):
         pi = self.problem.get_pi(s, self.action)
         mu = self.problem.get_mu(s, self.action)
         rho = pi / mu
-        x_p = self.problem.get_state_feature_rep(s_p)
+        if is_terminal:
+            x_p = np.zeros(self.problem.num_features)
+        else:
+            x_p = self.problem.get_state_feature_rep(s_p)
         x = self.problem.get_state_feature_rep(s)
         delta = rho * (r + self.gamma * np.dot(self.w, x_p) - np.dot(self.w, x))
         alpha = self.compute_step_size()
         self.w += alpha * delta * x
 
-
-class TDMultiplePolicy(BaseAgentLearnMultiplePolicies):
-    def learn(self, s, s_p, r):
+    def learn_multiple_policies(self, s, s_p, r, is_terminal):
         active_policies_vec = self.problem.get_active_policies(s)
         self.r_vec = np.zeros(self.problem.num_policies)
         if r > 0:
@@ -23,7 +24,10 @@ class TDMultiplePolicy(BaseAgentLearnMultiplePolicies):
             self.r_vec = r * terminal_policies_vec
         alpha_vec = active_policies_vec * self.compute_step_size()
         x = self.problem.get_state_feature_rep(s)
-        x_p = self.problem.get_state_feature_rep(s_p)
+        if is_terminal:
+            x_p = np.zeros(self.problem.num_features)
+        else:
+            x_p = self.problem.get_state_feature_rep(s_p)
         stacked_x = self.problem.stacked_feature_rep[:, :, s]
         pi = self.problem.get_pi(s, self.action)
         mu = self.problem.get_mu(s, self.action)
