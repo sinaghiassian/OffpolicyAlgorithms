f7e57c226e3b5c1a0c9a131e95b74ca03ceeb281
ali <akyegane@gmail.com>
2020-09-12 09:27:25 +0430

    add render test py

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 from collections import namedtuple
 
 Transition = namedtuple('Transition',
@@ -22,3 +23,11 @@ class ImmutableDict(dict):
 
     def clear(self) -> None:
         self.immutable()
+
+
+def generate_gif(frames, path):
+    import imageio
+    from skimage.transform import resize
+    for idx, frame_idx in enumerate(frames):
+        frames[idx] = resize(frame_idx, (256, 256, 3), preserve_range=True, order=0).astype(np.uint8)
+    imageio.mimsave(path, frames, duration=1 / 10)
