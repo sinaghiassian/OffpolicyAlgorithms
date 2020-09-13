ed903e1267a5762694baf0841d4924f31830ad6f
ali <akyegane@gmail.com>
2020-09-13 16:26:01 +0430

    add render class and ErrorRender class

--- Environments/rendering.py
+++ Environments/rendering.py
@@ -1,32 +1,40 @@
-import pyglet
-
-window = pyglet.window.Window(800, 600)
-
-
-episode_number_label = pyglet.text.Label('Hello, world',
-                          font_name='Times New Roman',
-                          font_size=36,
-                          x=window.width // 2, y=window.height // 2,
-                          anchor_x='center', anchor_y='center')
-@window.event
-def on_draw():
-    window.clear()
-    #label.draw()
-    player_ship.draw()
+from abc import ABC, abstractmethod
+import numpy as np
 
 
+class Render(ABC):
+    @abstractmethod
+    def render(self, img):
+        raise NotImplementedError
 
-from pyglet.gl import GLubyte
 
-pixels = [
-    255, 0, 0,      0, 255, 0,      0, 0, 255,     # RGB values range from
-    255, 0, 0,      255, 0, 0,      255, 0, 0,     # 0 to 255 for each color
-    255, 0, 0,      255, 0, 0,      255, 0, 0,     # component.
-]
-rawData = (GLubyte * len(pixels))(*pixels)
-imageData = pyglet.image.ImageData(3, 3, 'RGB', rawData)
-player_ship = pyglet.sprite.Sprite(img=imageData, x=400, y=300)
+class ErrorRender(Render):
+    def __init__(self, num_policies, num_steps):
+        self.num_steps = num_steps
+        self.num_policies = num_policies
+        self._error, self._max_error = None, None
 
-if __name__ == "__main__":
-    pyglet.app.run()
+    def render(self, img):
+        e = self._error[0]
+        x = self._max_error[0]
+        d = (e - x) * 300 * e
+        d = np.nan_to_num(d).reshape(11, 11).astype(np.uint8)
+        img[:, :, 1] += np.transpose(d)#((np.flip(np.transpose(d), axis=1)))
 
+        e = self._error[2]
+        x = self._max_error[2]
+        d = (e - x) * 300 * e
+        d = np.nan_to_num(d).reshape(11, 11).astype(np.uint8)
+        img[:, :, 2] += np.transpose(d)
+
+        e = self._error[6]
+        x = self._max_error[6]
+        d = (e - x) * 300 * e
+        d = np.nan_to_num(d).reshape(11, 11).astype(np.uint8)
+        img[:, :, 0] += np.transpose(d)
+        return img
+
+    def add_error(self, error):
+        if self._max_error is None:
+            self._max_error = np.abs(error)
+        self._error = np.abs(error)
