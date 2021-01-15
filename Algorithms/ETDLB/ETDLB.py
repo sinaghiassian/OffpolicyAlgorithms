bce3773dad34e57fb8e9d1d8b6f0a5d5158f57bf
sinaghiassian <ghiassia@ualberta.ca>
2021-01-15 16:46:36 -0500

    Coded ETDLambdaBeta.

---
+++ Algorithms/ETDLB/ETDLB.py
@@ -0,0 +1,23 @@
+from Algorithms.BaseGradient import BaseGradient
+import numpy as np
+
+
+class HTD(BaseGradient):
+    def __init__(self, task, **kwargs):
+        super().__init__(task, **kwargs)
+        self.F = 1
+        self.old_rho = 0
+        self.beta = kwargs.get('beta')
+
+    def learn_single_policy(self, s, s_p, r, is_terminal):
+        x, x_p = self.get_features(s, s_p, is_terminal)
+        delta = self.get_delta(r, x, x_p)
+        self.F = self.beta * self.old_rho * self.F + 1
+        m = self.lmbda * 1 + (1 - self.lmbda) * self.F
+        rho = self.get_isr(s)
+        self.z = rho * (x * m + self.gamma * self.lmbda * self.z)
+        self.w += self.compute_step_size() * delta * self.z
+        self.old_rho = rho
+
+    def learn_multiple_policies(self, s, s_p, r, is_terminal):
+        ...
