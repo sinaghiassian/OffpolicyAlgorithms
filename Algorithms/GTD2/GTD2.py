8ea561205c042fe36267c597a49a33307ab4f0c1
sinaghiassian <ghiassia@ualberta.ca>
2021-01-15 15:48:07 -0500

    Added new GradientTD methods.

--- Algorithms/GTD2/GTD2.py
+++ Algorithms/GTD2/GTD2.py
@@ -2,11 +2,11 @@ from Algorithms.BaseGradient import BaseGradient
 import numpy as np
 
 
-class GTD(BaseGradient):
+class GTD2(BaseGradient):
     def learn_single_policy(self, s, s_p, r, is_terminal):
         delta, alpha, x, x_p, *_ = super().learn_single_policy(s, s_p, r, is_terminal)
         alpha_v = self.compute_second_step_size()
-        self.w += alpha * (delta * self.z - self.gamma * (1 - self.lmbda) * np.dot(self.z, self.w) * x_p)
+        self.w += alpha * (np.dot(x, self.w) * x - self.gamma * (1 - self.lmbda) * np.dot(self.z, self.w) * x_p)
         self.v += alpha_v * (delta * self.z - np.dot(x, self.v) * x)
 
     def learn_multiple_policies(self, s, s_p, r, is_terminal):
