855c428b8b2df7481e115fba1449490da8969a21
sinaghiassian <ghiassia@ualberta.ca>
2021-01-15 15:47:20 -0500

    Added a base Gradient class.

---
+++ Algorithms/BaseGradient.py
@@ -0,0 +1,14 @@
+from abc import ABC
+import numpy as np
+from Algorithms.BaseTD import BaseTD
+from Tasks.BaseTask import BaseTask
+
+
+class BaseGradient(BaseTD, ABC):
+    def __init__(self, task: BaseTask, **kwargs):
+        super().__init__(task, **kwargs)
+        self.v = np.zeros(self.task.num_features)
+        self.alpha_v = kwargs.get('alpha_v')
+
+    def compute_second_step_size(self):
+        return self.alpha_v
