624220a5f59d79e83a2ca2bed4fdf67558c83807
sinaghiassian <ghiassia@ualberta.ca>
2021-01-18 14:59:41 -0500

    Moved task_dict its own file.

--- Registry/TaskRegistry.py
+++ Registry/TaskRegistry.py
@@ -1,2 +1,4 @@
 from Tasks.EightStateOffPolicyRandomFeat import EightStateOffPolicyRandomFeat
 from Tasks.LearnEightPoliciesTileCodingFeat import LearnEightPoliciesTileCodingFeat
+task_dict = {'EightStateOffPolicyRandomFeat': EightStateOffPolicyRandomFeat,
+                 'LearnEightPoliciesTileCodingFeat': LearnEightPoliciesTileCodingFeat}
