ca6a374b16d8ad646326f78660d0f06abf2af498
sinaghiassian <ghiassia@ualberta.ca>
2021-01-18 14:57:50 -0500

    Added the alg_dict here.

--- Registry/AlgRegistry.py
+++ Registry/AlgRegistry.py
@@ -7,3 +7,5 @@ from Algorithms.ETDLB.ETDLB import ETDLB
 from Algorithms.ABTD.ABTD import ABTD
 from Algorithms.Vtrace.Vtrace import Vtrace
 from Algorithms.TB.TB import TB
+alg_dict = {'TD': TD, 'GTD': GTD, 'GTD2': GTD2, 'PGTD2': PGTD2, 'HTD': HTD, 'ETDLB': ETDLB, 'ABTD': ABTD,
+            'Vtrace': Vtrace, 'TB': TB}
