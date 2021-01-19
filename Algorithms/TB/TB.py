996b9ec234f890252cd6e35733aca6b1539694d8
sinaghiassian <ghiassia@ualberta.ca>
2021-01-19 15:52:08 -0500

    Implemented and checked TreeBackup multi policy learning.

--- Algorithms/TB/TB.py
+++ Algorithms/TB/TB.py
@@ -9,4 +9,9 @@ class TB(BaseVariableLmbda):
         self.old_pi = pi
 
     def learn_multiple_policies(self, s, s_p, r, is_terminal):
-        ...
+        delta, alpha_vec, x, x_p, pi, mu, rho, stacked_x = super().learn_multiple_policies(s, s_p, r, is_terminal)
+        delta = rho * delta
+        self.z = (self.gamma_vec_t * self.lmbda * self.old_pi)[:, None] * self.z + stacked_x
+        self.w += alpha_vec[:, None] * (delta[:, None] * self.z)
+        self.old_pi = pi
+        self.gamma_vec_t = self.gamma_vec_tp
