cfc37d2656a6f9d94f471e14554b3f0deb202860
sinaghiassian <ghiassia@ualberta.ca>
2021-01-19 14:33:03 -0500

    Coded up Proximal-GTD2 algorithm. Works correctly.

--- Algorithms/PGTD2/PGTD2.py
+++ Algorithms/PGTD2/PGTD2.py
@@ -13,4 +13,16 @@ class PGTD2(BaseGradient):
         self.v += alpha_v * (delta_mid * self.z - np.dot(x, v_mid) * x)
 
     def learn_multiple_policies(self, s, s_p, r, is_terminal):
-        ...
+        delta, alpha_vec, x, x_p, rho, stacked_x, stacked_x_p, alphav_vec = super().learn_multiple_policies(
+            s, s_p, r, is_terminal)
+        self.z = rho[:, None] * (self.lmbda * self.z * self.gamma_vec_t[:, None] + stacked_x)
+        v_mid = self.v + alphav_vec[:, None] * (delta[:, None] * self.z - np.sum(x * self.v, 1)[:, None] * stacked_x)
+        phi_prime_multiplier = (1 - self.lmbda) * self.gamma_vec_tp * np.sum(self.z * self.v, 1)
+        w_mid = self.w + alpha_vec[:, None] * (
+                np.sum(x * self.v, 1)[:, None] * stacked_x - phi_prime_multiplier[:, None] * stacked_x_p)
+        delta_mid = self.r_vec + self.gamma_vec_tp * np.dot(w_mid, x_p) - np.dot(w_mid, x)
+        phi_prime_multiplier = (1 - self.lmbda) * self.gamma_vec_tp * np.sum(self.z * v_mid, 1)
+        self.w += alpha_vec[:, None] * (
+                np.sum(x * v_mid, 1)[:, None] * stacked_x - phi_prime_multiplier[:, None] * stacked_x_p)
+        self.v += alphav_vec[:, None] * (delta_mid[:, None] * self.z - np.sum(x * v_mid, 1)[:, None] * stacked_x)
+        self.gamma_vec_t = self.gamma_vec_tp
