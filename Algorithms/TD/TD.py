980ef2a63d91ce6e28fa7e580007b3d7af462c82
sinaghiassian <ghiassia@ualberta.ca>
2021-01-19 11:21:55 -0500

    TD for multiple policies new implementation moves stuff to the parent class to reduce redundancy.

--- Algorithms/TD/TD.py
+++ Algorithms/TD/TD.py
@@ -1,5 +1,4 @@
 from Algorithms.BaseTD import BaseTD
-import numpy as np
 
 
 class TD(BaseTD):
@@ -8,22 +7,8 @@ class TD(BaseTD):
         self.w += alpha * delta * self.z
 
     def learn_multiple_policies(self, s, s_p, r, is_terminal):
-        active_policies_vec = self.task.get_active_policies(s)
-        self.r_vec = np.zeros(self.task.num_policies)
-        if r > 0:
-            terminal_policies_vec = self.task.get_terminal_policies(s_p)
-            self.r_vec = r * terminal_policies_vec
-        alpha_vec = active_policies_vec * self.compute_step_size()
-        x = self.task.get_state_feature_rep(s)
-        x_p = np.zeros(self.task.num_features)
-        if not is_terminal:
-            x_p = self.task.get_state_feature_rep(s_p)
+        delta, alpha_vec, *_, rho = super().learn_multiple_policies(s, s_p, r, is_terminal)
         stacked_x = self.task.stacked_feature_rep[:, :, s]
-        pi = self.task.get_pi(s, self.action)
-        mu = self.task.get_mu(s, self.action)
-        rho = pi / mu
-        self.gamma_vec_tp = self.task.get_active_policies(s_p) * self.gamma
-        delta = self.r_vec + self.gamma_vec_tp * np.dot(self.w, x_p) - np.dot(self.w, x)
         self.z = rho[:, None] * (self.lmbda * self.z * self.gamma_vec_t[:, None] + stacked_x)
         self.w += (alpha_vec * delta)[:, None] * self.z
         self.gamma_vec_t = self.gamma_vec_tp
