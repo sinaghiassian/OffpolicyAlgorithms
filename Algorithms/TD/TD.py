fc2eafd259e4f63a508358eaed330f713e0ed984
sinaghiassian <ghiassia@ualberta.ca>
2021-01-19 12:11:32 -0500

    moved stacked feature rep into the parent class.

--- Algorithms/TD/TD.py
+++ Algorithms/TD/TD.py
@@ -7,8 +7,7 @@ class TD(BaseTD):
         self.w += alpha * delta * self.z
 
     def learn_multiple_policies(self, s, s_p, r, is_terminal):
-        delta, alpha_vec, *_, rho = super().learn_multiple_policies(s, s_p, r, is_terminal)
-        stacked_x = self.task.stacked_feature_rep[:, :, s]
+        delta, alpha_vec, *_, rho, stacked_x = super().learn_multiple_policies(s, s_p, r, is_terminal)
         self.z = rho[:, None] * (self.lmbda * self.z * self.gamma_vec_t[:, None] + stacked_x)
         self.w += (alpha_vec * delta)[:, None] * self.z
         self.gamma_vec_t = self.gamma_vec_tp
