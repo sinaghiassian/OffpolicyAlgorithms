f3af3d8accc2654647fe9f6d85e2640ac5f8c351
sinaghiassian <ghiassia@ualberta.ca>
2021-01-19 12:54:07 -0500

    Small change.

--- Algorithms/BaseGradient.py
+++ Algorithms/BaseGradient.py
@@ -8,9 +8,9 @@ class BaseGradient(BaseTD, ABC):
     def __init__(self, task: BaseTask, **kwargs):
         super().__init__(task, **kwargs)
         self.v = np.zeros(self.task.num_features)
+        self.alpha_v = kwargs.get('alpha_v')
         if self.task.num_policies > 1:
             self.v = np.zeros((self.task.num_policies, self.task.num_features))
-        self.alpha_v = kwargs.get('alpha_v')
 
     def compute_second_step_size(self):
         return self.alpha_v * self.compute_step_size()
