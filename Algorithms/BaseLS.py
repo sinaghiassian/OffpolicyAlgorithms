59e5fb879075ae4fce5edf156c70bd2cd15086ea
sinaghiassian <ghiassia@ualberta.ca>
2021-01-24 12:13:17 -0700

    LSTD and LSETD both are working correctly. Both inherit from BaseLS.py.

--- Algorithms/BaseLS.py
+++ Algorithms/BaseLS.py
@@ -10,28 +10,27 @@ class BaseLS(BaseTD):
         self.A = np.zeros((self.task.num_features, self.task.num_features))
         self.b = np.zeros(self.task.num_features)
         self.t = 0
+        if self.task.num_policies > 1:
+            self.A = np.zeros((self.task.num_policies, self.task.num_features, self.task.num_features))
+            self.b = np.zeros((self.task.num_policies, self.task.num_features))
+            self.gamma_vec_t = np.concatenate((np.ones(2), np.zeros(6))) * self.gamma
+            self.t = np.zeros(self.task.num_policies)
 
     def learn_single_policy(self, s, s_p, r, is_terminal):
         x, x_p = self.get_features(s, s_p, is_terminal)
         self.t += 1
-        self.A += (np.dot(self.z[:, None], (x - self.gamma * x_p)[:, None]) - self.A) / self.t
+        self.A += (np.outer(self.z, (x - self.gamma * x_p)) - self.A) / self.t
         self.b += (r * self.z - self.b) / self.t
         self.w = np.dot(pinv(self.A), self.b)
 
-
-class LSTD:
-    def learn(self, phi, phiPrime, r, gamma, policyParameters):
-        rho = policyParameters['rho']
-
-        self.e = rho * (self.gamma_t * self.lmbda * self.e + phi)
-
-        self.A = self.A + (np.dot(self.e[:, None], (phi - gamma * phiPrime)[None, :]) - self.A) / (self.timeStep + 1)
-        self.b = self.b + (r * self.e - self.b) / (self.timeStep + 1)
-
-        self.theta = np.dot(pinv(self.A), self.b)
-
-        self.gamma_t = gamma
-        self.timeStep = self.timeStep + 1
-
-    def episodeInit(self):
-        self.e = np.zeros(self.featureSize)
+    def learn_multiple_policies(self, s, s_p, r, is_terminal):
+        _, _, x, x_p, _, _, _, stacked_x = \
+            super(BaseLS, self).learn_multiple_policies(s, s_p, r, is_terminal)
+        for i in range(self.task.num_policies):
+            if self.gamma_vec_t[i] != 0.0:
+                self.t[i] += 1
+                z = self.z[i, :]
+                self.A[i, :, :] += (np.outer(z, (x - self.gamma_vec_tp[i] * x_p)) - self.A[i, :, :]) / self.t[i]
+                self.b[i, :] += (self.r_vec[i] * z - self.b[i, :]) / self.t[i]
+                self.w[i, :] = np.dot(pinv(self.A[i, :, :]), self.b[i, :])
+        self.gamma_vec_t = self.gamma_vec_tp
