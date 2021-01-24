63f45fad19038923db58136cb9c3d6449bfa86bb
sinaghiassian <ghiassia@ualberta.ca>
2021-01-24 12:11:56 -0700

    First correctly working version of LSTD for both single and multiple policies.

--- Algorithms/LSTD.py
+++ Algorithms/LSTD.py
@@ -3,5 +3,11 @@ from Algorithms.BaseLS import BaseLS
 
 class LSTD(BaseLS):
     def learn_single_policy(self, s, s_p, r, is_terminal):
-        self.z = self.get_isr(s) * (self.gamma * self.lmbda * self.z + self.get_features(s, s_p, is_terminal)[0])
+        x, _ = self.get_features(s, s_p, is_terminal)
+        self.z = self.get_isr(s) * (self.gamma * self.lmbda * self.z + x)
         super(LSTD, self).learn_single_policy(s, s_p, r, is_terminal)
+
+    def learn_multiple_policies(self, s, s_p, r, is_terminal):
+        x, _ = self.get_features(s, s_p, is_terminal)
+        self.z = self.get_isr(s)[:, None] * (self.lmbda * self.z * self.gamma_vec_t[:, None] + x)
+        super(LSTD, self).learn_multiple_policies(s, s_p, r, is_terminal)
