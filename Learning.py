e27006cbb974f8b1e187945ddaa5bdeec9572424
sinaghiassian <ghiassia@ualberta.ca>
2021-01-26 11:30:57 -0700

    Added a for loop to run all of the runs in the Learning.py script rather than submitting one job per run. This makes sense because tasks are small. Added num_of_runs and num_steps to command line arguments. Added RMSVE  and RMSVE_of_runs and RMSVE_of_run. Added code to save the results. The learning curve, and the area under the learning curve and final performance.

--- Learning.py
+++ Learning.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import argparse
 import random
+from utils import save_result
 from Registry.AlgRegistry import alg_dict
 from Registry.EnvRegistry import environment_dict
 from Registry.TaskRegistry import task_dict
@@ -17,43 +18,59 @@ if __name__ == '__main__':
     parser.add_argument('--zeta', '-z', type=float, default=default_params['meta_parameters']['zeta'])
     parser.add_argument('--algorithm', '-alg', type=str, default=default_params['agent'])
     parser.add_argument('--task', '-t', type=str, default=default_params['task'])
-    parser.add_argument('--run_number', '-r', type=int, default=default_params['meta_parameters']['run'])
+    parser.add_argument('--num_of_runs', '-nr', type=int, default=default_params['meta_parameters']['num_of_runs'])
+    parser.add_argument('--num_steps', '-ns', type=int, default=default_params['meta_parameters']['num_steps'])
     parser.add_argument('--environment', '-e', type=str, default=default_params['environment'])
     parser.add_argument('--save_path', '-sp', type=str, default='Results')
     parser.add_argument('--render', '-render', type=bool, default=False)
     args = parser.parse_args()
 
+    params = {'alpha': args.alpha, 'lmbda': args.lmbda, 'eta': args.eta, 'beta': args.beta, 'zeta': args.zeta}
     if not os.path.exists(args.save_path):
         os.makedirs(args.save_path)
 
-    np.random.seed(args.run_number)
-    random.seed(a=args.run_number)
+    RMSVE = np.zeros((task_dict[args.task].num_of_policies(), args.num_steps, args.num_of_runs))
+    RMSVE_of_runs = np.zeros((args.num_of_runs, args.num_steps))
+    for run in range(args.num_of_runs):
+        np.random.seed(run)
+        random.seed(a=run)
+        env = environment_dict[args.environment]()
+        task = task_dict[args.task](run_number=run, num_steps=args.num_steps)
+        agent = alg_dict[args.algorithm](task, **params)
 
-    env = environment_dict[args.environment]()
-    task = task_dict[args.task](run_number=args.run_number)
-    params = {'alpha': args.alpha, 'lmbda': args.lmbda, 'eta': args.eta, 'beta': args.beta, 'zeta': args.zeta}
-    agent = alg_dict[args.algorithm](task, **params)
+        RMSVE_of_run = np.zeros((task.num_policies, task.num_steps))
+        agent.state = env.reset()
+        is_terminal = False
+        error_render = ErrorRender(task.num_policies, task.num_steps)
+        for step in range(task.num_steps):
+            RMSVE_of_run[:, step], error = agent.compute_rmsve()
+            if args.render:
+                error_render.add_error(error)
+            agent.action = agent.choose_behavior_action()
+            agent.next_state, r, is_terminal, info = env.step(agent.action)
+            agent.learn(agent.state, agent.next_state, r, is_terminal)
+            if is_terminal:
+                agent.state = env.reset()
+                is_terminal = False
+                agent.reset()
+                continue
+            agent.state = agent.next_state
+            if args.render:
+                env.render(mode='screen', render_cls=error_render)
+        print(np.mean(RMSVE_of_run, axis=0))
+        RMSVE[:, :, run] = RMSVE_of_run
+    RMSVE_of_runs = np.transpose(np.mean(RMSVE, axis=0))  # Average over all policies.
+    save_result(args.save_path, '_RMSVE_mean_over_runs', np.mean(RMSVE_of_runs, axis=0), params)
+    save_result(args.save_path, '_stderr_over_runs', np.std(RMSVE_of_runs, axis=0, ddof=1) / np.sqrt(args.num_of_runs), params)
+    final_limit = int(0.01 * args.num_steps)
+    final_errors = RMSVE_of_runs[:, args.num_steps - final_limit - 1:]
+    final_errors_mean_over_steps = np.mean(final_errors, axis=1)
+    save_result(args.save_path, '_mean_stderr_final',
+                np.array([np.mean(final_errors_mean_over_steps),
+                          np.std(final_errors_mean_over_steps, ddof=1) / np.sqrt(args.num_runs)]), params)
+    auc_mean_over_steps = np.mean(RMSVE_of_runs, axis=1)
+    save_result(args.save_path, '_mean_stderr_auc',
+                np.array([np.mean(auc_mean_over_steps),
+                          np.std(auc_mean_over_steps, ddof=1) / np.sqrt(args.num_runs)]), params)
 
-    RMSVE = np.zeros((task.num_policies, task.num_steps))
-    agent.state = env.reset()
-    is_terminal = False
-    error_render = ErrorRender(task.num_policies, task.num_steps)
-    for step in range(task.num_steps):
-        RMSVE[:, step], error = agent.compute_rmsve()
-        if args.render:
-            error_render.add_error(error)
-        agent.action = agent.choose_behavior_action()
-        agent.next_state, r, is_terminal, info = env.step(agent.action)
-        agent.learn(agent.state, agent.next_state, r, is_terminal)
-        if is_terminal:
-            agent.state = env.reset()
-            is_terminal = False
-            agent.reset()
-            continue
-        agent.state = agent.next_state
-        if args.render:
-            env.render(mode='screen', render_cls=error_render)
-    print(np.mean(RMSVE, axis=0))
-
-# TODO: Collector: Save and load the data.
-# TODO: Add Plotting code.
+    # TODO: Add Plotting code.
