dbf3383b8ed42981c3b37c45b97ecdd8dabcc915
sinaghiassian <ghiassia@ualberta.ca>
2021-01-26 11:26:10 -0700

    removed run from the for loop that generates runs to be run on the server. The runs are now handled in a for loop in the Learning.py script. Also added num_steps to the arguments to be passed to Learning.py.

--- Job/Cedar_Create_Config_Template.sh
+++ Job/Cedar_Create_Config_Template.sh
@@ -5,7 +5,8 @@ lmbda=(__LMBDA__)
 eta=(__ETA__)
 beta=(__BETA__)
 zeta=(__ZETA__)
-run=({__RUN__})
+num_of_runs=(__NUMOFRUNS__)
+num_steps=(__NUMSTEPS__)
 algorithm=(__ALGORITHM__)
 environment=(__ENVIRONMENT__)
 task=(__TASK__)
@@ -16,10 +17,9 @@ for A in ${alpha[@]}; do
     for E in ${eta[@]}; do
       for B in ${beta[@]}; do
         for Z in ${zeta[@]}; do
-          for R in ${run[@]}; do
-            echo export SAVE_PATH=${save_path[*]} ENVIRONMENT=${environment[*]} ALGORITHM=${algorithm[*]} \
-            TASK=${task[*]} ALPHA=${A} LMBDA=${L} ETA=${E} BETA=${B} ZETA=${Z} RUN=${R} >>exports.dat
-          done
+          echo export SAVE_PATH=${save_path[0]} ENVIRONMENT=${environment[0]} ALGORITHM=${algorithm[0]} \
+          TASK=${task[0]} ALPHA=${A} LMBDA=${L} ETA=${E} BETA=${B} ZETA=${Z} NUMOFRUNS=${num_of_runs[0]} \
+          NUMSTEPS=${num_steps[0]} >>exports.dat
         done
       done
     done
