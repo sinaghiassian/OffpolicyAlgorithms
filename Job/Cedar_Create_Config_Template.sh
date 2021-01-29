ee6c28a0fd32815c8dbec2a30b80581eda8eddfd
sinaghiassian <ghiassia@ualberta.ca>
2021-01-29 16:28:48 -0700

    Some Changes.

--- Job/Cedar_Create_Config_Template.sh
+++ Job/Cedar_Create_Config_Template.sh
@@ -5,22 +5,22 @@ lmbda=(__LMBDA__)
 eta=(__ETA__)
 beta=(__BETA__)
 zeta=(__ZETA__)
-num_of_runs=(__NUMOFRUNS__)
-num_steps=(__NUMSTEPS__)
-sub_sample=(__SUBSAMPLE__)
-algorithm=(__ALGORITHM__)
-environment=(__ENVIRONMENT__)
-task=(__TASK__)
-save_path=(__SAVEPATH__)
+num_of_runs=__NUMOFRUNS__
+num_steps=__NUMSTEPS__
+sub_sample=__SUBSAMPLE__
+algorithm=__ALGORITHM__
+environment=__ENVIRONMENT__
+task=__TASK__
+save_path=__SAVEPATH__
 
 for A in ${alpha[@]}; do
   for L in ${lmbda[@]}; do
     for E in ${eta[@]}; do
       for B in ${beta[@]}; do
         for Z in ${zeta[@]}; do
-          echo export SAVE_PATH=${save_path[0]} ENVIRONMENT=${environment[0]} ALGORITHM=${algorithm[0]} \
-          TASK=${task[0]} ALPHA=${A} LMBDA=${L} ETA=${E} BETA=${B} ZETA=${Z} NUMOFRUNS=${num_of_runs[0]} \
-          NUMSTEPS=${num_steps[0]} SUBSAMPLE=${sub_sample[0]} >>exports_${algorithm[0]}.dat
+          echo export SAVE_PATH=${save_path} ENVIRONMENT=${environment} ALGORITHM=${algorithm} \
+          TASK=${task} ALPHA=${A} LMBDA=${L} ETA=${E} BETA=${B} ZETA=${Z} NUMOFRUNS=${num_of_runs} \
+          NUMSTEPS=${num_steps} SUBSAMPLE=${sub_sample} >>exports_${algorithm}.dat
         done
       done
     done
