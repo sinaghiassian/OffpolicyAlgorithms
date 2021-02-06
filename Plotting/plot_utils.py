bcb07d565d5e984698afd307cef272ade9251cca
sinaghiassian <ghiassia@ualberta.ca>
2021-02-06 14:55:51 -0700

    Added a plot_utils.py that has the functions that all plotting files have in common.

---
+++ Plotting/plot_utils.py
@@ -0,0 +1,57 @@
+import matplotlib.pyplot as plt
+import argparse
+import json
+import os
+
+from Job.JobBuilder import default_params
+from Registry.AlgRegistry import alg_dict
+
+
+def make_params(alg_name, exp_name):
+    params = dict()
+    alg_param_names = alg_dict[alg_name].related_parameters()
+    exp_path = os.path.join(os.getcwd(), '../Experiments', exp_name, alg_name, f'{alg_name}.json')
+    res_path = os.path.join(os.getcwd(), '../Results', exp_name, alg_name)
+    if not os.path.exists(exp_path):
+        return [], [], [], res_path
+    with open(exp_path) as f:
+        json_exp_params = json.load(f).get('meta_parameters')
+    for param in alg_param_names:
+        params[param] = json_exp_params.get(param, default_params['meta_parameters'][param])
+        if not isinstance(params[param], list):
+            params[param] = list([params[param]])
+    fp_list = params.get('alpha', params['alpha'])
+    tp_list = [0.0]
+    if 'eta' in params:
+        tp_list = params['eta']
+    elif 'beta' in params:
+        tp_list = params['beta']
+    if 'lmbda' in params:
+        sp_list = params['lmbda']
+    else:
+        sp_list = params['zeta']
+    return fp_list, sp_list, tp_list, res_path
+
+
+def make_current_params(alg_name, sp, tp):
+    current_params = {'alpha': 0}
+    alg_param_names = alg_dict[alg_name].related_parameters()
+    if 'lmbda' in alg_param_names:
+        current_params['lmbda'] = sp
+    else:
+        current_params['zeta'] = sp
+    if 'eta' in alg_param_names:
+        current_params['eta'] = tp
+    return current_params
+
+
+def make_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--exp_name', '-n', type=str, default='FirstChain')
+    return parser.parse_args()
+
+
+def make_fig():
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    return fig, ax
