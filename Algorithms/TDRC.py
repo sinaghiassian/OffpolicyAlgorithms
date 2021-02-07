79f2892ed0ac6d21f0261c2ab8419041da5ea760
sinaghiassian <ghiassia@ualberta.ca>
2021-02-07 09:19:52 -0700

    TDRC bug resolved.

--- Algorithms/TDRC.py
+++ Algorithms/TDRC.py
@@ -2,6 +2,7 @@ from Algorithms.GTD import GTD
 import numpy as np
 
 
+# noinspection DuplicatedCode
 class TDRC(GTD):
     def __init__(self, task, **kwargs):
         super().__init__(task, **kwargs)
@@ -10,10 +11,15 @@ class TDRC(GTD):
     def learn_single_policy(self, s, s_p, r, is_terminal):
         delta, alpha, x, x_p, _ = super().learn_single_policy(s, s_p, r, is_terminal)
         alpha_v = self.compute_second_step_size()
+        self.w += alpha * (delta * self.z - self.gamma * (1 - self.lmbda) * np.dot(self.z, self.v) * x_p)
         self.v += alpha_v * (delta * self.z - np.dot(x, self.v) * x) - alpha_v * self.tdrc_beta * self.v
 
     def learn_multiple_policies(self, s, s_p, r, is_terminal):
         delta, alpha_vec, x, x_p, *_, rho, stacked_x, stacked_x_p, alphav_vec = super().learn_multiple_policies(
             s, s_p, r, is_terminal)
+        self.z = rho[:, None] * (self.lmbda * self.z * self.gamma_vec_t[:, None] + stacked_x)
+        phi_prime_multiplier = (1 - self.lmbda) * self.gamma_vec_tp * np.sum(self.z * self.v, 1)
+        self.w += alpha_vec[:, None] * (delta[:, None] * self.z - phi_prime_multiplier[:, None] * stacked_x_p)
         self.v += alphav_vec[:, None] * (delta[:, None] * self.z - np.sum(
             x * self.v, 1)[:, None] * stacked_x) - alphav_vec * self.tdrc_beta * self.v
+        self.gamma_vec_t = self.gamma_vec_tp
