1130c6aa55b5fc59f80dd8bab972158b9ef42dee
sinaghiassian <ghiassia@ualberta.ca>
2021-02-09 18:55:10 -0700

    Added ETD algorithm to the registry. Also changed the order of the algorithms.

--- Registry/AlgRegistry.py
+++ Registry/AlgRegistry.py
@@ -5,10 +5,11 @@ from Algorithms.GTD2 import GTD2
 from Algorithms.PGTD2 import PGTD2
 from Algorithms.HTD import HTD
 from Algorithms.ETDLB import ETDLB
+from Algorithms.ETD import ETD
 from Algorithms.ABTD import ABTD
 from Algorithms.Vtrace import Vtrace
 from Algorithms.TB import TB
 from Algorithms.LSTD import LSTD
 from Algorithms.LSETD import LSETD
-alg_dict = {'TD': TD, 'GTD': GTD, 'TDRC': TDRC, 'GTD2': GTD2, 'PGTD2': PGTD2, 'HTD': HTD, 'ETDLB': ETDLB, 'ABTD': ABTD,
-            'Vtrace': Vtrace, 'TB': TB, 'LSTD': LSTD, 'LSETD': LSETD}
+alg_dict = {'TD': TD, 'ETD': ETD, 'Vtrace': Vtrace, 'HTD': HTD, 'GTD': GTD, 'TB': TB, 'GTD2': GTD2, 'ABTD': ABTD,
+            'ETDLB': ETDLB, 'PGTD2': PGTD2, 'TDRC': TDRC, 'LSTD': LSTD, 'LSETD': LSETD}
