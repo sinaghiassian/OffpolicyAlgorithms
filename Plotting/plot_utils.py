83777345b3a5a0369e162f48787a4cf2c1c5acf1
sinaghiassian <ghiassia@ualberta.ca>
2021-02-11 08:34:47 -0700

    Removed makefig function. resolved a bug where if a directory does not exist, we raise an exception. Made get attributes class instead of dictionary.

--- Plotting/plot_utils.py
+++ Plotting/plot_utils.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import argparse
 import json
+import numpy as np
 import os
 from Job.JobBuilder import default_params
 from Registry.AlgRegistry import alg_dict
@@ -60,14 +61,8 @@ def make_current_params(alg_name, sp, tp, fop):
 
 def make_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--exp_name', '-n', type=str, default='FirstChain')
+    parser.add_argument('--exp_name', '-n', type=str, default='FirstFourRoom')
     return parser.parse_args()
-
-
-def make_fig():
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    return fig, ax
 
 
 def get_alg_names(exp_name):
@@ -79,7 +74,7 @@ def get_alg_names(exp_name):
 def load_sample_json_for_exp(exp_name):
     alg_name = get_alg_names(exp_name)[0]
     exp_path = os.path.join(os.getcwd(), '../Experiments', exp_name, alg_name, f'{alg_name}.json')
-    if os.path.exists(exp_path):
+    if not os.path.exists(exp_path):
         print('No algorithms exist in the experiment directory...')
         raise FileExistsError
     with open(exp_path) as f:
@@ -87,16 +82,54 @@ def load_sample_json_for_exp(exp_name):
     return json_exp_params
 
 
-def get_attr_for_learning_curve(exp_name):
-    attr_dict = dict()
-    json_exp_params = load_sample_json_for_exp(exp_name)
-    if exp_name == 'FirstChain':
-        attr_dict = {'y_lim': [0.0, 0.8], 'x_lim': [0.0, json_exp_params['number_of_steps']],
-                     'y_axis_ticks': [0.1, 0.3, 0.5, 0.7], 'x_axis_ticks': [0.0, 10000, 20000],
-                     'x_tick_labels': [0, '10K', '20K']}
-    elif exp_name == 'FirstFourRoom':
-        attr_dict = {'y_lim': [0.0, 0.8], 'x_lim': [0.0, json_exp_params['number_of_steps']],
-                     'y_axis_ticks': [0.1, 0.3, 0.5, 0.7],
-                     'x_axis_ticks': [0.0, 10000, 20000, 30000, 40000, 50000],
-                     'x_tick_labels': [0, '10K', '20K', '30K', '40K', '50K']}
-    return attr_dict
+# def get_attr(exp_name):
+#     attr_dict = dict()
+#     json_exp_params = load_sample_json_for_exp(exp_name)
+#     if exp_name == 'FirstChain':
+#         attr_dict = {'y_lim': [0.0, 0.8], 'x_lim': [0.0, json_exp_params['number_of_steps']],
+#                      'y_axis_ticks': [0.1, 0.3, 0.5, 0.7], 'x_axis_ticks': [0.0, 10000, 20000],
+#                      'x_tick_labels': [0, '10K', '20K'], 'over_limit_replacement': 2.0, 'over_limit_waterfall': 0.79,
+#                      'learning_starting_point': 0.6891}
+#     elif exp_name == 'FirstFourRoom':
+#         attr_dict = {'y_lim': [0.0, 0.8], 'x_lim': [0.0, json_exp_params['number_of_steps']],
+#                      'y_axis_ticks': [0.1, 0.3, 0.5, 0.7],
+#                      'x_axis_ticks': [0.0, 10000, 20000, 30000, 40000, 50000],
+#                      'x_tick_labels': [0, '10K', '20K', '30K', '40K', '50K'], 'over_limit_replacement': 2.0,
+#                      'over_limit_waterfall': 0.79, 'learning_starting_point': 0.7268}
+#     return attr_dict
+
+
+class FirstChainAttr:
+    def __init__(self, exp_name):
+        json_exp_params = load_sample_json_for_exp(exp_name)
+        self.size_of_labels = 25
+        self.y_lim = [0.0, 0.8]
+        self.x_lim = [0.0, json_exp_params['number_of_steps']]
+        self.y_axis_ticks = [0.1, 0.3, 0.5, 0.7]
+        self.x_axis_ticks = [0.0, 10000, 20000]
+        self.x_tick_labels = [0, '10', '20']
+        self.over_limit_replacement = 2.0
+        self.over_limit_waterfall = 0.79
+        self.learning_starting_point = 0.6891
+
+
+class FirstFourRoomAttr:
+    def __init__(self, exp_name):
+        json_exp_params = load_sample_json_for_exp(exp_name)
+        self.size_of_labels = 25
+        self.y_lim = [0.0, 0.8]
+        self.x_lim = [0.0, json_exp_params['number_of_steps']]
+        self.y_axis_ticks = [0.1, 0.3, 0.5, 0.7]
+        self.x_axis_ticks = [0.0, 10000, 20000, 30000, 40000, 50000]
+        self.x_tick_labels = [0, '10', '20', '30', '40', '50']
+        self.over_limit_replacement = 2.0
+        self.over_limit_waterfall = 0.79
+        self.learning_starting_point = 0.7268
+
+
+attr_dict = {'FirstChain': FirstChainAttr, 'FirstFourRoom': FirstFourRoomAttr}
+
+
+def replace_large_nan_inf(arr, large=1.0, replace_with=2.0):
+    arr[np.isnan(arr)], arr[np.isinf(arr)], arr[arr > large] = replace_with, replace_with, replace_with
+    return arr
