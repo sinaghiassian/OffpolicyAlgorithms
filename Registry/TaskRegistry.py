b2787ff6c0466b4d828c6aa963834e215e349661
sinaghiassian <ghiassia@ualberta.ca>
2021-02-15 14:00:06 -0700

    Added high variance four rooms to task registry.

--- Registry/TaskRegistry.py
+++ Registry/TaskRegistry.py
@@ -1,4 +1,6 @@
 from Tasks.EightStateOffPolicyRandomFeat import EightStateOffPolicyRandomFeat
 from Tasks.LearnEightPoliciesTileCodingFeat import LearnEightPoliciesTileCodingFeat
+from Tasks.HighVarianceLearnEightPoliciesTileCodingFeat import HighVarianceLearnEightPoliciesTileCodingFeat
 task_dict = {'EightStateOffPolicyRandomFeat': EightStateOffPolicyRandomFeat,
-             'LearnEightPoliciesTileCodingFeat': LearnEightPoliciesTileCodingFeat}
+             'LearnEightPoliciesTileCodingFeat': LearnEightPoliciesTileCodingFeat,
+             'HighVarianceLearnEightPoliciesTileCodingFeat': HighVarianceLearnEightPoliciesTileCodingFeat}
