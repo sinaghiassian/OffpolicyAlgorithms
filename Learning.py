b3d38233a9f65a3cb3e52731b11bb9059085928d
sinaghiassian <ghiassia@ualberta.ca>
2021-02-28 19:46:18 -0700

    Learn is now a function. Mainly so we can run it from different places, for example to use it to rerun the best results for avoiding maximization bias.˚

--- Learning.py
+++ Learning.py
@@ -1,13 +1,64 @@
 import os
 import numpy as np
 import argparse
-import random
-from utils import save_result
+from utils import save_result, Configuration
 from Registry.AlgRegistry import alg_dict
 from Registry.EnvRegistry import environment_dict
 from Registry.TaskRegistry import task_dict
 from Job.JobBuilder import default_params
 from Environments.rendering import ErrorRender
+
+
+def learn(config: Configuration):
+    params = dict()
+    for k, v in config.items():
+        if k in alg_dict[config.algorithm].related_parameters():
+            params[k] = v
+
+    if not os.path.exists(config.save_path):
+        os.makedirs(config.save_path, exist_ok=True)
+
+    rmsve = np.zeros((task_dict[config.task].num_of_policies(), config.num_steps, config.num_of_runs))
+    for run in range(config.num_of_runs):
+        random_seed = (run + config.num_of_runs) if config.rerun else run
+        np.random.seed(random_seed)
+        env = environment_dict[config.environment]()
+        task = task_dict[config.task](run_number=run, num_steps=config.num_steps)
+        agent = alg_dict[config.algorithm](task, **params)
+
+        rmsve_of_run = np.zeros((task.num_policies, task.num_steps))
+        agent.state = env.reset()
+        error_render = ErrorRender(task.num_policies, task.num_steps)
+        for step in range(task.num_steps):
+            rmsve_of_run[:, step], error = agent.compute_rmsve()
+            if config.render:
+                error_render.add_error(error)
+            agent.action = agent.choose_behavior_action()
+            agent.next_state, r, is_terminal, info = env.step(agent.action)
+            agent.learn(agent.state, agent.next_state, r, is_terminal)
+            if is_terminal:
+                agent.state = env.reset()
+                agent.reset()
+                continue
+            agent.state = agent.next_state
+            if config.render:
+                env.render(mode='screen', render_cls=error_render)
+        print(np.mean(rmsve_of_run, axis=0))
+        rmsve[:, :, run] = rmsve_of_run
+    rmsve_of_runs = np.transpose(np.mean(rmsve, axis=0))  # Average over all policies.
+    save_result(config.save_path, '_RMSVE_mean_over_runs', np.mean(rmsve_of_runs, axis=0), params, config.rerun)
+    save_result(config.save_path, '_RMSVE_stderr_over_runs',
+                np.std(rmsve_of_runs, axis=0, ddof=1) / np.sqrt(config.num_of_runs), params, config.rerun)
+    final_errors_mean_over_steps = np.mean(rmsve_of_runs[:, config.num_steps - int(0.01 * config.num_steps) - 1:],
+                                           axis=1)
+    save_result(config.save_path, '_mean_stderr_final',
+                np.array([np.mean(final_errors_mean_over_steps), np.std(final_errors_mean_over_steps, ddof=1) /
+                          np.sqrt(config.num_of_runs)]), params, config.rerun)
+    auc_mean_over_steps = np.mean(rmsve_of_runs, axis=1)
+    save_result(config.save_path, '_mean_stderr_auc',
+                np.array([np.mean(auc_mean_over_steps),
+                          np.std(auc_mean_over_steps, ddof=1) / np.sqrt(config.num_of_runs)]), params, config.rerun)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -24,60 +75,11 @@ if __name__ == '__main__':
     parser.add_argument('--sub_sample', '-ss', type=int, default=default_params['sub_sample'])
     parser.add_argument('--environment', '-e', type=str, default=default_params['environment'])
     parser.add_argument('--save_path', '-sp', type=str, default='-')
-    parser.add_argument('--render', '-render', type=bool, default=False)
+    parser.add_argument('--rerun', '-rrn', type=bool, default=False)
+    parser.add_argument('--render', '-rndr', type=bool, default=False)
     args = parser.parse_args()
     if args.save_path == '-':
         args.save_path = os.path.join(os.getcwd(), 'Results', args.task, args.algorithm)
 
-    all_params = {'alpha': args.alpha, 'lmbda': args.lmbda, 'eta': args.eta, 'beta': args.beta, 'zeta': args.zeta,
-                  'tdrc_beta': args.tdrc_beta}
-    params = dict()
-    for k, v in all_params.items():
-        if k in alg_dict[args.algorithm].related_parameters():
-            params[k] = v
-
-    if not os.path.exists(args.save_path):
-        os.makedirs(args.save_path, exist_ok=True)
-
-    RMSVE = np.zeros((task_dict[args.task].num_of_policies(), args.num_steps, args.num_of_runs))
-    RMSVE_of_runs = np.zeros((args.num_of_runs, args.num_steps))
-    for run in range(args.num_of_runs):
-        np.random.seed(run)
-        random.seed(a=run)
-        env = environment_dict[args.environment]()
-        task = task_dict[args.task](run_number=run, num_steps=args.num_steps)
-        agent = alg_dict[args.algorithm](task, **params)
-
-        RMSVE_of_run = np.zeros((task.num_policies, task.num_steps))
-        agent.state = env.reset()
-        is_terminal = False
-        error_render = ErrorRender(task.num_policies, task.num_steps)
-        for step in range(task.num_steps):
-            RMSVE_of_run[:, step], error = agent.compute_rmsve()
-            if args.render:
-                error_render.add_error(error)
-            agent.action = agent.choose_behavior_action()
-            agent.next_state, r, is_terminal, info = env.step(agent.action)
-            agent.learn(agent.state, agent.next_state, r, is_terminal)
-            if is_terminal:
-                agent.state = env.reset()
-                is_terminal = False
-                agent.reset()
-                continue
-            agent.state = agent.next_state
-            if args.render:
-                env.render(mode='screen', render_cls=error_render)
-        # print(np.mean(RMSVE_of_run, axis=0))
-        RMSVE[:, :, run] = RMSVE_of_run
-    RMSVE_of_runs = np.transpose(np.mean(RMSVE, axis=0))  # Average over all policies.
-    save_result(args.save_path, '_RMSVE_mean_over_runs', np.mean(RMSVE_of_runs, axis=0), params)
-    save_result(args.save_path, '_RMSVE_stderr_over_runs',
-                np.std(RMSVE_of_runs, axis=0, ddof=1) / np.sqrt(args.num_of_runs), params)
-    final_errors_mean_over_steps = np.mean(RMSVE_of_runs[:, args.num_steps - int(0.01 * args.num_steps) - 1:], axis=1)
-    save_result(args.save_path, '_mean_stderr_final',
-                np.array([np.mean(final_errors_mean_over_steps),
-                          np.std(final_errors_mean_over_steps, ddof=1) / np.sqrt(args.num_of_runs)]), params)
-    auc_mean_over_steps = np.mean(RMSVE_of_runs, axis=1)
-    save_result(args.save_path, '_mean_stderr_auc',
-                np.array([np.mean(auc_mean_over_steps),
-                          np.std(auc_mean_over_steps, ddof=1) / np.sqrt(args.num_of_runs)]), params)
+    config = Configuration(vars(args))
+    learn(config=config)
