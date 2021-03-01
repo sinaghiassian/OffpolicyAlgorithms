027a15c6da7a05dbfeab9b9f8af66caa824a30ba
sinaghiassian <ghiassia@ualberta.ca>
2021-03-01 09:26:10 -0700

    Added plot_waterfall_scatter function.

--- plot_data.py
+++ plot_data.py
@@ -1,7 +1,9 @@
 from Plotting.plot_learning_curve import plot_learning_curve
 from Plotting.plot_sensitivity import plot_sensitivity_curve
+from Plotting.plot_waterfall import plot_waterfall_scatter
 from process_data import process_data
 
 # process_data()
 # plot_learning_curve()
-plot_sensitivity_curve()
+# plot_sensitivity_curve()
+plot_waterfall_scatter()
