e14ca067d7172a1d3bc8ec389bca5ed498692eaf
sinaghiassian <ghiassia@ualberta.ca>
2021-02-28 19:36:36 -0700

    Added a new script to the root that handles all plotting scripts in the Plot directory. This also has the order in which one should run the plotting files.

---
+++ plot_data.py
@@ -0,0 +1,7 @@
+from Plotting.plot_learning_curve import plot_learning_curve
+from Plotting.plot_sensitivity import plot_sensitivity_curve
+from process_data import process_data
+
+# process_data()
+# plot_learning_curve()
+plot_sensitivity_curve()
