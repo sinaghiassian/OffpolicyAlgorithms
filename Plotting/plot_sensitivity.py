a3aa78d95afb90a9c9f5d3e23c2d1d6fdef4a743
sinaghiassian <ghiassia@ualberta.ca>
2021-03-02 11:56:40 -0700

    Now handles plotting reran data and original data, or only reran data, or only original data, by user's choice.

--- Plotting/plot_sensitivity.py
+++ Plotting/plot_sensitivity.py
@@ -3,24 +3,23 @@ import numpy as np
 import json
 import matplotlib.pyplot as plt
 
-from Plotting.plot_params import EXPS, ALG_GROUPS, ALG_COLORS, EXP_ATTRS, AUC_AND_FINAL, LMBDA_AND_ZETA
-from Plotting.plot_utils import make_current_params, replace_large_nan_inf, load_best_rerun_params, make_res_path, \
-    make_exp_path
+from Plotting.plot_params import EXPS, ALG_GROUPS, ALG_COLORS, EXP_ATTRS, AUC_AND_FINAL, LMBDA_AND_ZETA, \
+    PLOT_RERUN_AND_ORIG, RERUN, RERUN_POSTFIX
+from Plotting.plot_utils import replace_large_nan_inf, make_res_path, make_exp_path, load_best_rerun_params_dict
 from utils import create_name_for_save_load
 
 
-def load_best_performance(alg, exp, auc_or_final, sp, best_tp, best_fop, exp_attrs):
-    current_params = make_current_params(alg, sp, best_tp, best_fop)
+def load_best_performance_over_alpha(alg, exp, auc_or_final, best_params, exp_attrs, postfix=''):
     res_path = make_res_path(alg, exp)
     load_file_name = os.path.join(res_path, create_name_for_save_load(
-        current_params, excluded_params=['alpha']) + f'_mean_{auc_or_final}_over_alpha.npy')
+        best_params, excluded_params=['alpha']) + f'_mean_{auc_or_final}_over_alpha{postfix}.npy')
     performance_over_alpha = np.load(load_file_name)
     performance_over_alpha = replace_large_nan_inf(
         performance_over_alpha, large=exp_attrs.learning_starting_point,
         replace_with=exp_attrs.over_limit_replacement)
     stderr_load_file_name = os.path.join(
-        res_path, create_name_for_save_load(current_params, excluded_params=['alpha']) +
-        f'_stderr_{auc_or_final}_over_alpha.npy')
+        res_path, create_name_for_save_load(best_params, excluded_params=['alpha']) +
+        f'_stderr_{auc_or_final}_over_alpha{postfix}.npy')
     std_err_of_best_perf_over_alpha = np.load(stderr_load_file_name)
     std_err_of_best_perf_over_alpha = replace_large_nan_inf(
         std_err_of_best_perf_over_alpha, large=exp_attrs.learning_starting_point, replace_with=0.0)
@@ -28,13 +27,16 @@ def load_best_performance(alg, exp, auc_or_final, sp, best_tp, best_fop, exp_att
 
 
 # noinspection DuplicatedCode
-def plot_sensitivity(ax, alg, alphas, best_performance, stderr, best_tp, best_fop, exp_attrs):
-    lbl = f'{alg}_{best_tp}_{best_fop}'
+def plot_sensitivity(ax, alg, alphas, best_performance, stderr, exp_attrs, second_time=False):
+    alpha = 1.0
+    if PLOT_RERUN_AND_ORIG:
+        alpha = 1.0 if second_time else 0.5
+    lbl = f'{alg}'
     ax.set_xscale('log', basex=2)
     ax.plot(alphas, best_performance, label=lbl, linestyle='-', marker='o', color=ALG_COLORS[alg],
-            linewidth=2, markersize=5)
+            linewidth=2, markersize=5, alpha=alpha)
     ax.errorbar(alphas, best_performance, yerr=stderr, ecolor=ALG_COLORS[alg], mfc=ALG_COLORS[alg],
-                mec=ALG_COLORS[alg], linestyle='', elinewidth=2, markersize=5)
+                mec=ALG_COLORS[alg], linestyle='', elinewidth=2, markersize=5, alpha=alpha)
     # ax.legend()
     ax.get_xaxis().tick_bottom()
     ax.get_yaxis().tick_left()
@@ -65,15 +67,25 @@ def plot_sensitivity_curve():
                 for alg_names in ALG_GROUPS.values():
                     fig, ax = plt.subplots()
                     for alg in alg_names:
-                        _, best_tp, best_fop = load_best_rerun_params(alg, exp, auc_or_final, sp)
-                        best_performance, stderr = load_best_performance(
-                            alg, exp, auc_or_final, sp, best_tp, best_fop, exp_attrs)
+                        postfix = RERUN_POSTFIX if RERUN else ''
+                        best_params = load_best_rerun_params_dict(alg, exp, auc_or_final, sp)
                         alphas = get_alphas(alg, exp)
-                        plot_sensitivity(ax, alg, alphas, best_performance, stderr, best_tp, best_fop, exp_attrs)
+                        best_performance, stderr = load_best_performance_over_alpha(
+                            alg, exp, auc_or_final, best_params, exp_attrs, postfix)
+                        plot_sensitivity(ax, alg, alphas, best_performance, stderr, exp_attrs)
+                        if PLOT_RERUN_AND_ORIG:
+                            best_performance, stderr = load_best_performance_over_alpha(
+                                alg, exp, auc_or_final, best_params, exp_attrs, postfix)
+                            plot_sensitivity(ax, alg, alphas, best_performance, stderr, exp_attrs, True)
                     if not os.path.exists(save_dir):
                         os.makedirs(save_dir, exist_ok=True)
-                    fig.savefig(save_dir + '/sensitivity_' + '_'.join(alg_names) + '.pdf', format='pdf', dpi=1000,
-                                bbox_inches='tight')
+                    if PLOT_RERUN_AND_ORIG:
+                        postfix = '_rerun_and_original'
+                    elif RERUN:
+                        postfix = RERUN_POSTFIX
+                    else:
+                        postfix = ''
+                    fig.savefig(os.path.join(save_dir, f"sensitivity_{'_'.join(alg_names)}{postfix}.pdf"),
+                                format='pdf', dpi=1000, bbox_inches='tight')
                     plt.show()
                     print(exp, alg_names, auc_or_final, sp)
-                    # TODO: add rerun.
