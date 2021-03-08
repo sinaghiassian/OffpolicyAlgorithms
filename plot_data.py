28efce6b45a601f73632f61fd1d5802e321ec599
sinaghiassian <ghiassia@ualberta.ca>
2021-03-07 19:31:45 -0700

    Added the new functions for plotting new learning and sensitivity curves.

--- plot_data.py
+++ plot_data.py
@@ -1,9 +1,13 @@
+from Plotting.plot_all_third_parameters import plot_all_sensitivities_per_alg
 from Plotting.plot_learning_curve import plot_learning_curve
+from Plotting.plot_learning_for_two_lambdas import plot_learning_curve_for_lambdas
 from Plotting.plot_sensitivity import plot_sensitivity_curve
 from Plotting.plot_waterfall import plot_waterfall_scatter
 from process_data import process_data
 
-process_data()
+# process_data()
 # plot_learning_curve()
-plot_sensitivity_curve()
+# plot_sensitivity_curve()
 # plot_waterfall_scatter()
+# plot_all_sensitivities_per_alg()
+plot_learning_curve_for_lambdas()
