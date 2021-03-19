e3f705db94bbc9b062f677a60f52fd2773d471c6
sinaghiassian <ghiassia@ualberta.ca>
2021-03-19 12:07:52 -0600

    Code to plot the behavior distribution for each task.

--- Plotting/plot_dist.py
+++ Plotting/plot_dist.py
@@ -19,18 +19,27 @@ def plot_d_mu(ax, d_mu, active_states):
     ax.set_xticklabels(x_labels)
 
 
+def find_active_states(task, d_mu, state_values, policy_no=0):
+    if task == 'EightStateOffPolicyRandomFeat':
+        return [x for x in range(d_mu.shape[0])]
+    return np.where(state_values[policy_no] > 0)[0]
+
+
+def get_active_d_mu(task, d_mu, active_states, policy_no=0):
+    if task == 'EightStateOffPolicyRandomFeat':
+        return d_mu
+    return d_mu[active_states, policy_no].squeeze()
+
+
 def plot_distribution():
-    task = 'EightStateOffPolicyRandomFeat'
+    task = 'HighVarianceLearnEightPoliciesTileCodingFeat'
     d_mu = load_d_mu(task)
     state_values = load_state_values(task)
     for policy_no in range(state_values.shape[0]):
         fig, ax = plt.subplots()
-        if task == 'EightStateOffPolicyRandomFeat':
-            active_states = [x for x in range(d_mu.shape[0])]
-            plot_d_mu(ax, d_mu, active_states)
-            plt.show()
-            break
-        active_states = np.where(state_values[policy_no] > 0)[0]
-        active_d_mu = d_mu[active_states, policy_no].squeeze()
+        active_states = find_active_states(task, d_mu, state_values, policy_no)
+        active_d_mu = get_active_d_mu(task, d_mu, active_states, policy_no)
         plot_d_mu(ax, active_d_mu, active_states)
         plt.show()
+        if task == 'EightStateOffPolicyRandomFeat':
+            break
