e5ea6e54749ef38ff9cac326c404766fc88a5279
sinaghiassian <ghiassia@ualberta.ca>
2021-03-19 12:11:51 -0600

    Printing negative tick labels on the x axis.

--- Plotting/plot_utils.py
+++ Plotting/plot_utils.py
@@ -142,7 +142,7 @@ class FirstChainAttr:
         self.x_axis_ticks = [0.0, 5000, 10000, 15000, 20000]
         self.x_tick_labels = [0, '5', '10', '15', '20']
         self.x_axis_ticks_log = [pow(2, -16), pow(2, -13), pow(2, -10), pow(2, -7), pow(2, -4), pow(2, -1)]
-        self.x_axis_tick_labels_log = [16, 13, 10, 7, 4, 1]
+        self.x_axis_tick_labels_log = [-16, -13, -10, -7, -4, -1]
         self.over_limit_replacement = 2.0
         self.over_limit_waterfall = 0.79
         self.learning_starting_point = 0.68910
@@ -159,7 +159,7 @@ class FirstFourRoomAttr:
         self.x_axis_ticks = [0.0, 10000, 20000, 30000, 40000, 50000]
         self.x_tick_labels = [0, '10', '20', '30', '40', '50']
         self.x_axis_ticks_log = [pow(2, -16), pow(2, -13), pow(2, -10), pow(2, -7), pow(2, -4), pow(2, -1)]
-        self.x_axis_tick_labels_log = [16, 13, 10, 7, 4, 1]
+        self.x_axis_tick_labels_log = [-16, -13, -10, -7, -4, -1]
         self.over_limit_replacement = 2.0
         self.over_limit_waterfall = 0.79
         self.learning_starting_point = 0.72672
