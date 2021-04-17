12a8825aa1a713f5928d3ea2ff9c18b45b633f9e
ali <akyegane@gmail.com>
2021-04-17 13:06:09 +0430

    #NewFeature: add gif generator for four Room grid world

--- test.py
+++ test.py
@@ -25,25 +25,25 @@ if __name__ == "__main__":
     frame = env.render(mode=render_mode)
     frames.append(frame)
     is_terminal = False
-    s_a = [0, 2, 2, 2, 2, 2, 2, 3, 3, 3, 2, 2, 2]
-    for step in range(len(s_a)):
+    #s_a = [0, 2, 2, 2, 2, 2, 2, 3, 3, 0, 0, 0, 0, 0, 0, 1, 2, 1, 0, 0, 3, 3, 3]
+    for step in range(50):
         # a = task.select_target_action(state, policy_id=0)
-        # a = np.random.randint(0, 4)
-        a = s_a[step]
+        a = np.random.randint(0, 4)
+        #a = s_a[step]
         next_state, r, is_terminal, info = env.step(a)
         x, y, x_p, y_p, is_rand, selected_action = info.values()
-        print(
-            f'sept:{step}, '
-            f's({state}):({x},{y}), '
-            f'a:{actions[a]}, '
-            f'environment_action: {actions[selected_action]}, '
-            f's_p({next_state}):({x_p},{y_p}), '
-            f'stochasticity:{is_rand}, '
-            f'terminal:{is_terminal}'
-        )
+        # print(
+        #     f'sept:{step}, '
+        #     f's({state}):({x},{y}), '
+        #     f'a:{actions[a]}, '
+        #     f'environment_action: {actions[selected_action]}, '
+        #     f's_p({next_state}):({x_p},{y_p}), '
+        #     f'stochasticity:{is_rand}, '
+        #     f'terminal:{is_terminal}'
+        # )
         state = next_state
         frame = env.render(mode=render_mode)
         frames.append(frame)
         if is_terminal:
             break
-        utils.generate_gif(frames, 'fourRoomGridWorld.gif')
+    utils.generate_gif(frames, 'Assets/fourRoomGridWorld.gif')
