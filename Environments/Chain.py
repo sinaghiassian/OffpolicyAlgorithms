7c38011d0d95b894f0c3238658ae17565cf82fb7
ali <akyegane@gmail.com>
2021-04-17 14:23:57 +0430

    #NewFeature: add screen and RGB render for chain environment

--- Environments/Chain.py
+++ Environments/Chain.py
@@ -12,6 +12,7 @@ class Chain:
         self.RIGHT_ACTION = 0
         self.RETREAT_ACTION = 1
         self.num_states = states_number
+        self._window = None
 
     def reset(self):
         self._state = np.random.randint(0, self._start_state_number)
@@ -29,9 +30,9 @@ class Chain:
         return self._state, 0, False, {}
 
     def render(self, mode='human'):
-        import sys
-        from Environments.utils import colorize
         if mode == 'human':
+            import sys
+            from Environments.utils import colorize
             corridor_map = [
                 str(i) if i > self._start_state_number
                 else colorize(str(i), "blue", highlight=False)
@@ -42,14 +43,53 @@ class Chain:
 
             sys.stdout.write(f'{"|".join(corridor_map)}\n')
 
+        if mode == "rgb" or mode == "screen":
+            RGB_COLORS = {
+                'red': np.array([240, 52, 52]),
+                'black': np.array([0, 0, 0]),
+                'green': np.array([77, 181, 33]),
+                'blue': np.array([29, 111, 219]),
+                'purple': np.array([112, 39, 195]),
+                'yellow': np.array([217, 213, 104]),
+                'grey': np.array([192, 195, 196]),
+                'light_grey': np.array([230, 230, 230]),
+                'white': np.array([255, 255, 255])
+            }
+            img = np.zeros((self.num_states, 1, 3), dtype=np.uint8)
+            img[:, 0] = RGB_COLORS['grey']
+            img[:self._start_state_number - 1, 0] = RGB_COLORS['yellow']
+            img[self._terminal - 1, 0] = RGB_COLORS['black']
+            img[self._state - 1, 0] = RGB_COLORS['green']
+
+            img = np.transpose(img, (1, 0, 2))
+            if mode == "screen":
+                from pyglet.window import Window
+                from pyglet.text import Label
+                from pyglet.gl import GLubyte
+                from pyglet.image import ImageData
+                zoom = 50
+                if self._window is None:
+                    self._window = Window(self.num_states * zoom, 1 * zoom)
+
+                dt = np.kron(img, np.ones((zoom, zoom, 1)))
+                dt = (GLubyte * dt.size)(*dt.flatten().astype('uint8'))
+                texture = ImageData(self._window.width, self._window.height, 'RGB', dt).get_texture()
+                self._window.clear()
+                self._window.switch_to()
+                self._window.dispatch_events()
+                texture.blit(0, 0)
+                # self._info.draw()
+                self._window.flip()
+            return np.flip(img, axis=0)
+
 
 if __name__ == '__main__':
     env = Chain()
     env.reset()
-    for step in range(1, 100):
+    for step in range(1, 1000):
         action = np.random.randint(0, 2)
         sp, r, terminal, _ = env.step(action=action)
-        env.render()
+        env.render(mode="screen")
         if terminal:
             env.reset()
             print('env reset')
