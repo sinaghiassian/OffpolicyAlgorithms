5e51f2c8ece6aec9ec42a5092e6d6e1a1bf85984
sinaghiassian <ghiassia@ualberta.ca>
2021-04-26 10:30:16 -0600

    Changes in the plots of the paper.

--- plot_data.py
+++ plot_data.py
@@ -1,4 +1,5 @@
 from Plotting.plot_all_sensitivities_per_alg_gradients import plot_all_sensitivities_per_alg_gradients
+from Plotting.plot_all_sensitivities_per_alg_gradients_all_eta import plot_all_sensitivities_per_alg_gradients_all_eta
 from Plotting.plot_dist import plot_distribution, plot_dist_for_two_four_room_tasks
 from Plotting.plot_all_sensitivities_per_alg_emphatics import plot_all_sensitivities_per_alg_emphatics
 from Plotting.plot_learning_curve import plot_learning_curve
@@ -10,15 +11,22 @@ from Plotting.plot_specific_learning_curves import plot_specific_learning_curves
 from Plotting.plot_waterfall import plot_waterfall_scatter
 from process_data import process_data
 
-func_to_run = 'high_variance_and_normal_dist_comparison'
-exps = ['FirstChain']
-if func_to_run == 'process_data':
-    exps = ['FirstChain', 'FirstFourRoom', '1HVFourRoom']
-    algs = ['TD', 'GTD', 'GTD2', 'PGTD2', 'HTD', 'TDRC', 'ETD', 'ETDLB', 'TB', 'Vtrace', 'ABTD']
-    auc_or_final = ['auc', 'final']
-    sp_list = [0.0, 0.9]
-    process_data(exps=exps, algs=algs, auc_or_final=auc_or_final, sp_list=sp_list)
 
+func_to_run = 'collision_sensitivity_curves_for_many_lambdas'
+exps = ['FirstChain']
+
+# region process data
+if func_to_run == 'process_data':
+    exps = ['FirstChain']
+    # algs = ['TD', 'GTD', 'GTD2', 'PGTD2', 'HTD', 'TDRC', 'ETD', 'ETDLB', 'TB', 'Vtrace', 'ABTD']
+    algs = ['TD', 'GTD', 'GTD2', 'PGTD2', 'HTD', 'TDRC', 'ETD', 'ETDLB']
+    auc_or_final = ['auc', 'final']
+    # sp_list = [0.0, 0.1, 0.2, 0.3, 0.5, 0.75, 0.875, 0.9375, 0.96875, 0.984375, 1.0]
+    sp_list = [0.1, 0.2, 0.3]
+    process_data(exps=exps, algs=algs, auc_or_final=auc_or_final, sp_list=sp_list)
+# endregion
+
+# region learning curves
 if func_to_run == 'collision_learning_curves_for_all_extra_params_full_bootstrapping':
     algs = ['GTD2', 'PGTD2', 'LSTD']
     sp_list = [0.0]
@@ -27,15 +35,6 @@ if func_to_run == 'collision_learning_curves_for_all_extra_params_full_bootstrap
     tp_list = [0.015625, 0.0625, 0.25, 1.0, 4.0, 16.0, 64.0, 256.0]
     plot_all_learning_curves_for_third(exps=exps, algs=algs, sp_list=sp_list, fig_size=fig_size,
                                        auc_or_final=auc_or_final, tp_list=tp_list)
-
-if func_to_run == 'collision_sensitivity_curves_for_two_lambdas':
-    sp_list = [0.0, 0.9]
-    fig_size = (6, 4)
-    algs = ['TD', 'GTD', 'GTD2', 'PGTD2', 'HTD', 'TDRC', 'ETD', 'ETDLB', 'TB', 'Vtrace', 'ABTD']
-    auc_or_final = ['auc']
-    plot_sensitivity_for_lambdas(exps=exps, algs=algs, sp_list=sp_list, fig_size=fig_size,
-                                 auc_or_final=auc_or_final)
-
 if func_to_run == 'collision_learning_curve_for_two_lambdas':
     sp_list = [0.0, 0.9]
     fig_size = (6, 4)
@@ -43,23 +42,6 @@ if func_to_run == 'collision_learning_curve_for_two_lambdas':
     auc_or_final = ['auc']
     plot_learning_curve_for_lambdas(exps=exps, alg_groups=alg_groups, sp_list=sp_list, fig_size=fig_size,
                                     auc_or_final=auc_or_final)
-
-if func_to_run == 'collision_best_sensitivity_curves_full_bootstrapping' or 'collision_waterfall_full_bootstrap':
-    sp_list = [0.0]
-    fig_size = (10, 4)
-    alg_groups = {'main_algs': ['TD', 'GTD', 'ETD'],
-                  'gradients': ['GTD', 'GTD2', 'HTD', 'PGTD2', 'TDRC'],
-                  'emphatics': ['ETD', 'ETDLB'],
-                  'fast_algs': ['TD', 'TB', 'Vtrace', 'ABTD'],
-                  'all_algs': ['TD', 'GTD', 'GTD2', 'PGTD2', 'HTD', 'TDRC', 'ETD', 'ETDLB', 'TB', 'Vtrace', 'ABTD']}
-    auc_or_final = ['auc']
-    if func_to_run == 'collision_best_sensitivity_curves_full_bootstrapping':
-        plot_sensitivity_curve(exps=exps, alg_groups=alg_groups, sp_list=sp_list, fig_size=fig_size,
-                               auc_or_final=auc_or_final)
-    elif func_to_run == 'collision_waterfall_full_bootstrap':
-        plot_waterfall_scatter(exps=exps, alg_groups=alg_groups, sp_list=sp_list, fig_size=fig_size,
-                               auc_or_final=auc_or_final)
-
 if func_to_run == 'collision_best_learning_curves_full_bootstrap':
     sp_list = [0.0]
     fig_size = (10, 4)
@@ -71,38 +53,42 @@ if func_to_run == 'collision_best_learning_curves_full_bootstrap':
                                'LSTD', 'LSETD']}
     auc_or_final = ['auc']
     plot_learning_curve(exps=exps, alg_groups=alg_groups, sp_list=sp_list, fig_size=fig_size, auc_or_final=auc_or_final)
-
-if func_to_run == 'state_dist':
-    fig_size = (6, 4)
-    tasks = ['EightStateOffPolicyRandomFeat', 'LearnEightPoliciesTileCodingFeat',
-             'HighVarianceLearnEightPoliciesTileCodingFeat']
-    for task in tasks:
-        plot_distribution(task=task, fig_size=fig_size)
-
-if func_to_run == 'high_variance_and_normal_dist_comparison':
-    fig_size = (6, 4)
-    plot_dist_for_two_four_room_tasks(fig_size=fig_size)
-
-if func_to_run == 'collision_gradients_sensitivity_full_bootstrap':
-    sp_list = [0.0]
-    fig_size = (8, 4)
-    algs = ['GTD', 'GTD2', 'PGTD2', 'HTD']
-    auc_or_final = ['auc']
-    plot_all_sensitivities_per_alg_gradients(exps=exps, algs=algs, sp_list=sp_list, fig_size=fig_size,
-                                             auc_or_final=auc_or_final)
-
-if func_to_run == 'collision_emphatics_sensitivity_full_bootstrap':
+if func_to_run == 'collision_best_learning_curves_some_algs_full_bootstrap':
     sp_list = [0.0]
     fig_size = (6, 4)
+    alg_groups = {'all_algs': ['TD', 'PGTD2', 'HTD', 'ETD', 'TB', 'Vtrace', 'ABTD']}
     auc_or_final = ['auc']
-    plot_all_sensitivities_per_alg_emphatics(exps=exps, sp_list=sp_list, fig_size=fig_size, auc_or_final=auc_or_final)
-
-if func_to_run == 'collision_emphatics_sensitivity_minimal_bootstrap':
+    plot_learning_curve(exps=exps, alg_groups=alg_groups, sp_list=sp_list, fig_size=fig_size, auc_or_final=auc_or_final,
+                        is_smoothed=True, smoothing_window=1)
+if func_to_run == 'collision_best_learning_curves_some_algs_medium_bootstrap':
+    sp_list = [0.5]
+    fig_size = (6, 4)
+    alg_groups = {'all_algs': ['TD', 'PGTD2', 'HTD', 'ETD', 'TB', 'Vtrace', 'ABTD']}
+    auc_or_final = ['auc']
+    plot_learning_curve(exps=exps, alg_groups=alg_groups, sp_list=sp_list, fig_size=fig_size, auc_or_final=auc_or_final,
+                        is_smoothed=True, smoothing_window=500)
+if func_to_run == 'collision_best_learning_curves_some_algs_minimal_bootstrap':
     sp_list = [0.9]
     fig_size = (6, 4)
+    alg_groups = {'all_algs': ['TD', 'PGTD2', 'HTD', 'ETD', 'TB', 'Vtrace', 'ABTD']}
     auc_or_final = ['auc']
-    plot_all_sensitivities_per_alg_emphatics(exps=exps, sp_list=sp_list, fig_size=fig_size, auc_or_final=auc_or_final)
-
+    plot_learning_curve(exps=exps, alg_groups=alg_groups, sp_list=sp_list, fig_size=fig_size, auc_or_final=auc_or_final,
+                        is_smoothed=True, smoothing_window=500)
+if func_to_run == 'collision_best_learning_curves_some_algs_no_bootstrap':
+    sp_list = [1.0]
+    fig_size = (6, 4)
+    alg_groups = {'all_algs': ['TD', 'PGTD2', 'HTD', 'ETD', 'TB', 'Vtrace', 'ABTD']}
+    auc_or_final = ['auc']
+    plot_learning_curve(exps=exps, alg_groups=alg_groups, sp_list=sp_list, fig_size=fig_size, auc_or_final=auc_or_final,
+                        is_smoothed=True, smoothing_window=500)
+if func_to_run == 'collision_best_learning_curves_full_bootstrap_rerun_and_original':  # also need to set RERUN = False
+    # and PLOT_RERUN_AND_ORIG = True in plot_params. Also some changes are necessary in the plot_learning_curve function
+    # like setting the colors and stuff for the re-run and original plots.
+    sp_list = [0.0]
+    fig_size = (10, 4)
+    alg_groups = {'all_algs': ['GTD']}
+    auc_or_final = ['final']
+    plot_learning_curve(exps=exps, alg_groups=alg_groups, sp_list=sp_list, fig_size=fig_size, auc_or_final=auc_or_final)
 if func_to_run == 'specific_learning_curves_full_bootstrap':
     auc_or_final = ['auc']
     fig_size = (10, 4)
@@ -146,6 +132,84 @@ if func_to_run == 'specific_learning_curves_full_bootstrap':
         }
         plot_specific_learning_curves(exp=exp, algs=algs, sp=sp, fig_size=fig_size, auc_or_final=auc_or_final,
                                       specific_params=specific_params)
+# endregion
+
+# region sensitivity curves
+if func_to_run == 'collision_TDRC_all_eta_one_beta':
+    sp_list = [0.0]
+    tdrc_beta = [0.01]  # possible values are 0.1, 0.01, 1.0. Set them separately to plot.
+    fig_size = (10, 6)
+    algs = ['TDRC']
+    auc_or_final = ['auc']
+    plot_all_sensitivities_per_alg_gradients_all_eta(exps=exps, algs=algs, sp_list=sp_list, fig_size=fig_size,
+                                                     auc_or_final=auc_or_final, tdrc_beta=tdrc_beta)
+if func_to_run == 'collision_sensitivity_curves_for_two_lambdas':
+    sp_list = [0.0, 0.9]
+    fig_size = (6, 4)
+    algs = ['TD', 'GTD', 'GTD2', 'PGTD2', 'HTD', 'TDRC', 'ETD', 'ETDLB', 'TB', 'Vtrace', 'ABTD']
+    auc_or_final = ['auc']
+    plot_sensitivity_for_lambdas(exps=exps, algs=algs, sp_list=sp_list, fig_size=fig_size,
+                                 auc_or_final=auc_or_final)
+if func_to_run == 'collision_sensitivity_curves_for_many_lambdas':
+    sp_list = [0.0, 0.1, 0.2, 0.3, 0.5, 0.75, 0.875, 0.9375, 0.96875, 0.984375, 1.0]
+    fig_size = (10, 4)
+    algs = ['TD', 'GTD', 'GTD2', 'PGTD2', 'HTD', 'TDRC', 'ETD', 'ETDLB', 'TB', 'Vtrace', 'ABTD']
+    # algs = ['TB', 'Vtrace', 'ABTD']
+    auc_or_final = ['auc']
+    plot_sensitivity_for_lambdas(exps=exps, algs=algs, sp_list=sp_list, fig_size=fig_size,
+                                 auc_or_final=auc_or_final)
+if func_to_run == 'collision_best_sensitivity_curves_full_bootstrapping' or 'collision_waterfall_full_bootstrap':
+    sp_list = [0.0]
+    fig_size = (10, 4)
+    alg_groups = {'main_algs': ['TD', 'GTD', 'ETD'],
+                  'gradients': ['GTD', 'GTD2', 'HTD', 'PGTD2', 'TDRC'],
+                  'emphatics': ['ETD', 'ETDLB'],
+                  'fast_algs': ['TD', 'TB', 'Vtrace', 'ABTD'],
+                  'all_algs': ['TD', 'GTD', 'GTD2', 'PGTD2', 'HTD', 'TDRC', 'ETD', 'ETDLB', 'TB', 'Vtrace', 'ABTD']}
+    auc_or_final = ['auc']
+    if func_to_run == 'collision_best_sensitivity_curves_full_bootstrapping':
+        plot_sensitivity_curve(exps=exps, alg_groups=alg_groups, sp_list=sp_list, fig_size=fig_size,
+                               auc_or_final=auc_or_final)
+    elif func_to_run == 'collision_waterfall_full_bootstrap':
+        plot_waterfall_scatter(exps=exps, alg_groups=alg_groups, sp_list=sp_list, fig_size=fig_size,
+                               auc_or_final=auc_or_final)
+if func_to_run == 'collision_gradients_sensitivity_full_bootstrap':
+    sp_list = [0.0]
+    fig_size = (8, 4)
+    algs = ['GTD', 'GTD2', 'PGTD2', 'HTD']
+    auc_or_final = ['auc']
+    plot_all_sensitivities_per_alg_gradients(exps=exps, algs=algs, sp_list=sp_list, fig_size=fig_size,
+                                             auc_or_final=auc_or_final)
+if func_to_run == 'collision_gradients_sensitivity_full_bootstrap_all_eta':
+    sp_list = [0.0]
+    fig_size = (10, 6)
+    algs = ['GTD', 'GTD2', 'PGTD2', 'HTD']
+    auc_or_final = ['auc']
+    plot_all_sensitivities_per_alg_gradients_all_eta(exps=exps, algs=algs, sp_list=sp_list, fig_size=fig_size,
+                                                     auc_or_final=auc_or_final)
+if func_to_run == 'collision_emphatics_sensitivity_full_bootstrap':
+    sp_list = [0.0]
+    fig_size = (6, 4)
+    auc_or_final = ['auc']
+    plot_all_sensitivities_per_alg_emphatics(exps=exps, sp_list=sp_list, fig_size=fig_size, auc_or_final=auc_or_final)
+if func_to_run == 'collision_emphatics_sensitivity_minimal_bootstrap':
+    sp_list = [0.9]
+    fig_size = (6, 4)
+    auc_or_final = ['auc']
+    plot_all_sensitivities_per_alg_emphatics(exps=exps, sp_list=sp_list, fig_size=fig_size, auc_or_final=auc_or_final)
+# endregion
+
+# region Misc
+if func_to_run == 'state_dist':
+    fig_size = (6, 4)
+    tasks = ['EightStateOffPolicyRandomFeat', 'LearnEightPoliciesTileCodingFeat',
+             'HighVarianceLearnEightPoliciesTileCodingFeat']
+    for task in tasks:
+        plot_distribution(task=task, fig_size=fig_size)
+if func_to_run == 'high_variance_and_normal_dist_comparison':
+    fig_size = (6, 4)
+    plot_dist_for_two_four_room_tasks(fig_size=fig_size)
+# endregion
 
 # from Plotting.process_state_value_function import plot_value_functions, plot_all_final_value_functions
 # from Tasks.HighVarianceLearnEightPoliciesTileCodingFeat import HighVarianceLearnEightPoliciesTileCodingFeat
