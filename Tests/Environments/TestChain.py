17a77f543e129b46ae484efd19cfbb9290b49da9
ali <akyegane@gmail.com>
2021-05-22 08:18:20 +0430

    #Refacotr change unit test name based on MethodName_ExpectedBehavior_StateUnderTest convention

--- Tests/Environments/TestChain.py
+++ Tests/Environments/TestChain.py
@@ -11,16 +11,16 @@ class TestChain(unittest.TestCase):
     def tearDown(self) -> None:
         self.env.reset()
 
-    def test_rest(self):
+    def test_rest_initial_state_between_zero_three(self):
         self.env.reset()
         self.assertIn(self.env._state, [0, 1, 2, 3])
 
-    def test_step_retreat(self):
+    def test_step_retreat_move_state_to_initial_state(self):
         self.env.reset()
         sp, r, is_done, _ = self.env.step(self.env.RETREAT_ACTION)
         self.assertEqual(is_done, True)
 
-    def test_step_right(self):
+    def test_step_right_move_state_one_step_to_right(self):
         self.env.reset()
         s = self.env._state
         sp, r, is_done, _ = self.env.step(self.env.RIGHT_ACTION)
