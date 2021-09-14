230203490c1546a0cf116900119170689877616f
Ali Khalili <akyegane@gmail.com>
2021-09-14 11:52:54 +0430

    NewFeature: added find_best_performance

--- data_presister.py
+++ data_presister.py
@@ -8,6 +8,8 @@ from typing import List, Optional, Dict
 import numpy as np
 
 from Job.JobBuilder import default_params
+from Plotting.plot_params import EXP_ATTRS
+from Plotting.plot_utils import load_and_replace_large_nan_inf
 from Registry.AlgRegistry import alg_dict
 from utils import Configuration
 
@@ -46,7 +48,7 @@ def group_dicts_by_first_key(list_of_dicts: List[Dict[str, float]]) -> Dict[str,
     return dict(final_grouped)
 
 
-def group_dicts_over_first_key(list_of_dicts: List[Dict[str, float]]) -> Dict[Dict[str, float], List[float]]:
+def group_dicts_over_first_key(list_of_dicts: List[Dict[str, float]]) -> Dict[tuple, List[float]]:
     """
     >>> group_dicts_over_first_key([{'alpha': 1, 'lambda': 4, 'gamma': 6}, {'alpha': 1, 'lambda': 5, 'gamma': 6}, {'alpha': 2, 'lambda': 4, 'gamma': 6}, {'alpha': 2, 'lambda': 5, 'gamma': 6}, {'alpha': 3, 'lambda': 4, 'gamma': 6}, {'alpha': 3, 'lambda': 5, 'gamma': 6}])
     {(('lambda', 4), ('gamma', 6)): [1, 2, 3], (('lambda', 5), ('gamma', 6)): [1, 2, 3]}
@@ -62,6 +64,44 @@ def group_dicts_over_first_key(list_of_dicts: List[Dict[str, float]]) -> Dict[Di
         final_grouped[tuple(inner_dict.items())].append(first_value)
 
     return dict(final_grouped)
+
+
+def find_best_performance(exp_name, alg_name, second_param, auc_or_final) -> Dict[str, float]:
+    exp_attrs = EXP_ATTRS[exp_name](exp_name)
+    best_params = {}
+    best_perf = np.inf
+
+    all_configuration = JsonParameterBuilder().add_experiment(exp_name).add_algorithm(alg_name).build()
+    list_of_configuration = split_dict_of_list_to_dicts(all_configuration)
+    first_param_key = get_first_key_of_dictionary(all_configuration)
+
+    grouped_over_first = group_dicts_over_first_key(list_of_configuration)
+
+    for grouped, first_values in grouped_over_first.items():
+        second_param_name, second_param_value = grouped[0]
+        if second_param_value != second_param:
+            continue
+        grouped_params = dict(grouped)
+        current_params = Configuration(grouped_params)
+        current_params[first_param_key] = None
+        current_params.algorithm = alg_name
+        current_params.save_path = PathFactory.make_result_path(exp_name, alg_name)
+        current_params.rerun = False
+
+        current_configuration_over_first_full_path = DataPersister.create_full_path_file_name(f'_mean_{auc_or_final}_over_alpha', current_params,
+                                                                                              excluded_params=[first_param_key])
+
+        current_perf = load_and_replace_large_nan_inf(
+            current_configuration_over_first_full_path, large=exp_attrs.learning_starting_point, replace_with=exp_attrs.over_limit_replacement)
+
+        min_perf = min(current_perf)
+        if min_perf < best_perf:
+            best_perf = min_perf
+            best_perf_idx = int(np.nanargmin(current_perf))
+            best_params = current_params
+            best_params[first_param_key] = first_values[best_perf_idx]
+
+    return best_params
 
 
 def get_first_key_of_dictionary(d: dict) -> str:
@@ -116,14 +156,16 @@ class PathFactory:
 
     @staticmethod
     def make_result_path(exp_name, alg_name):
-        return os.path.join(os.getcwd(), 'Results', exp_name, alg_name, f'{alg_name}.json')
+        return os.path.join(os.getcwd(), 'Results', exp_name, alg_name)
 
 
 class DataPersister:
 
     @staticmethod
-    def save_result(result_arr: np.ndarray, result_name: str, configuration: Configuration):
-        full_path_file_to_save = DataPersister.create_full_path_file_name(result_name, configuration)
+    def save_result(result_arr: np.ndarray, result_name: str, configuration: Configuration, excluded_params: Optional[list] = None):
+        full_path_file_to_save = DataPersister.create_full_path_file_name(result_name, configuration, excluded_params)
+        if not os.path.exists(os.path.dirname(full_path_file_to_save)):
+            os.makedirs(os.path.dirname(full_path_file_to_save))
         np.save(full_path_file_to_save, result_arr)
 
     @staticmethod
@@ -138,24 +180,26 @@ class DataPersister:
 
         for grouped, first_values in grouped_over_first.items():
             grouped_params = dict(grouped)
-            print('------------------------------------------------')
-            print(grouped_params)
-            for index, first_value in enumerate(first_values):
-                grouped_params[first_param_key] = first_value
-                current_params = Configuration(grouped_params)
-                current_params.algorithm = alg_name
-                current_params.save_path = PathFactory.make_result_path(exp_name, alg_name)
-                current_params.rerun = False
+            current_params = Configuration(grouped_params)
+            current_params[first_param_key] = None
+            current_params.algorithm = alg_name
+            current_params.save_path = PathFactory.make_result_path(exp_name, alg_name)
+            current_params.rerun = False
 
+            for index, first_value in enumerate(first_values):
+                current_params[first_param_key] = first_value
                 full_path_file_to_save = DataPersister.create_full_path_file_name(f'_mean_stderr_{auc_or_final}', current_params)
                 # perf = np.load(full_path_file_to_save)
                 # mean_over_alpha[index], stderr_over_alpha[index] = perf[0], perf[1]
-                print(full_path_file_to_save)
+
+            # TODO: in case the rerun postfix is needed it should implement
+            DataPersister.save_result(mean_over_alpha, f"_mean_{auc_or_final}_over_alpha", current_params, excluded_params=[first_param_key])
+            DataPersister.save_result(stderr_over_alpha, f"_stderr_{auc_or_final}_over_alpha", current_params, excluded_params=[first_param_key])
 
     @staticmethod
-    def create_full_path_file_name(result_name: str, configuration: Configuration) -> str:
+    def create_full_path_file_name(result_name: str, configuration: Configuration, excluded_params: Optional[list] = None) -> str:
         params = ParameterBuilder().add_algorithm_params(configuration).build()
-        file_name_to_save = DataPersister.create_file_name(params, excluded_params=None)
+        file_name_to_save = DataPersister.create_file_name(params, excluded_params=excluded_params)
         full_path_file_to_save = os.path.join(configuration.save_path, file_name_to_save)
         full_path_file_to_save = f'{full_path_file_to_save}{result_name}'
         if configuration.rerun:
--- test.py
+++ test.py
@@ -8,7 +8,7 @@ import pyglet
 from skimage.transform import resize
 import numpy as np
 
-from data_presister import DataPersister
+from data_presister import DataPersister, find_best_performance
 
 # if __name__ == "__main__":
 # render_mode = 'human'
@@ -31,4 +31,7 @@ from data_presister import DataPersister
 #     if is_terminal:
 #         env.reset()
 # utils.generate_gif(frames, 'Assets/FourRoomGridWorld.gif', size=(180, 180, 3), duration=1 / 20)
-DataPersister.save_best_pref_over_first_param(exp_name="FirstChain", alg_name="HTD",auc_or_final="auc")
+
+# DataPersister.save_best_pref_over_first_param(exp_name="FirstChain", alg_name="HTD", auc_or_final="auc")
+
+find_best_performance(exp_name="FirstChain", alg_name="HTD", auc_or_final="auc", second_param=0.2)
